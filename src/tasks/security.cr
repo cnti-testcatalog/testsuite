6f11aaee10382dcaac4acc222c8fc03eae6a865a
wolfpack <wolfpack@vulk.co>
2020-02-12 02:55:43 -0500

    #cncf/cnf-conformance/issues/18 Test categories now structured from the top down

---
+++ src/tasks/security.cr
@@ -0,0 +1,11 @@
+require "sam"
+require "file_utils"
+require "colorize"
+require "totem"
+require "./utils.cr"
+
+desc "CNF containers should be isolated from one another and the host.  The CNF Conformance suite uses tools like Falco, Sysdig Inspect and gVisor"
+task "security" do |_, args|
+end
+
+
