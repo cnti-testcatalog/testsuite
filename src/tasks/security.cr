0772f5850dec7a648314609fc7655a5aec180515
denverwilliams <denver@debian.nz>
2020-02-26 19:12:08 -0500

    #cncf/cnf-conformance/issues/55 privileged test now checks for helm_chart_container_name and white list

--- src/tasks/security.cr
+++ src/tasks/security.cr
@@ -18,13 +18,20 @@ task "privileged" do |_, args|
   #TODO Get list of priviliged containers
   #TODO Remove containers that are on white list
   #TODO If privileged containers count > 0 Error, else pass
+  config = cnf_conformance_yml
+  helm_chart_container_name = config.get("helm_chart_container_name").as_s
+  white_list_container_name = config.get("white_list_helm_chart_container_names").as_a
+  puts "helm_chart_container_name #{helm_chart_container_name}" if check_verbose(args)
+  puts "white_list_container_name #{white_list_container_name.inspect}" if check_verbose(args)
   privileged_response = `kubectl get pods --all-namespaces -o jsonpath='{.items[*].spec.containers[?(@.securityContext.privileged==true)].name}'`
   puts "privileged_response #{privileged_response}" if check_verbose(args)
   privileged_list = privileged_response.to_s.split(" ").uniq
   puts "privileged_list #{privileged_list}" if check_verbose(args)
-  if privileged_list.size == 0 
-    puts "PASSED: No privileged containers".colorize(:green)
+  violation_list = (privileged_list - white_list_container_name)
+  if privileged_list.find {|x| x == helm_chart_container_name} ||
+      violation_list.size > 0
+    puts "FAILURE: #{violation_list.size} privileged containers: #{violation_list.inspect}".colorize(:red)
   else
-    puts "FAILURE: #{privileged_list.size} privileged containers!".colorize(:red)
+    puts "PASSED: No privileged containers".colorize(:green)
   end
 end
