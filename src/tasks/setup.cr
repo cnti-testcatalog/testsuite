362589baf698cfb4cd73712523175042dcb124d8
denverwilliams <denver@debian.nz>
2020-03-05 16:41:52 -0500

    #cncf/cnf-conformance/issues/80 setup now installs a local version of helm

--- src/tasks/setup.cr
+++ src/tasks/setup.cr
@@ -4,6 +4,6 @@ require "colorize"
 require "totem"
 
 desc "Sets up the CNF Conformance test suite, the K8s cluster, and upstream projects"
-task "setup", ["install_opa" , "install_api_snoop", "install_sonobuoy", "install_chart_testing", "sample_coredns_setup"] do  |_, args|
+task "setup", ["install_opa" , "install_api_snoop", "install_sonobuoy", "install_chart_testing", "sample_coredns_setup", "helm_local_cleanup"] do  |_, args|
 end
 
