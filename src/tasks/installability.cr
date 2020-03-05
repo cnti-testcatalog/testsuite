014bea21b486a8bdf7e4319a5f9a9606bafd4283
denverwilliams <denver@debian.nz>
2020-03-05 16:24:57 -0500

    #cncf/cnf-conformance/issues/80 helm_chart_valid now has helm_local_install as a prereq

--- src/tasks/installability.cr
+++ src/tasks/installability.cr
@@ -37,7 +37,7 @@ task "install_script_helm" do |_, args|
   end
 end
 
-task "helm_chart_valid", ["install_script_helm"] do |_, args|
+task "helm_chart_valid", ["helm_local_install"] do |_, args|
   begin
     puts "increase_capacity args.raw: #{args.raw}" if check_verbose(args)
     puts "increase_capacity args.named: #{args.named}" if check_verbose(args)
