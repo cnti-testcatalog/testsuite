4a96384e0ab6af567d416466e315948ea095b97d
denverwilliams <denver@debian.nz>
2020-03-05 13:30:35 -0500

    #cncf/cnf-conformance/issues/80 Test now exists for CNF setup wait

---
+++ spec/sample_coredns_setup_spec.cr
@@ -0,0 +1,24 @@
+require "./spec_helper"
+require "colorize"
+require "../src/tasks/utils.cr"
+require "file_utils"
+
+describe "Utils" do
+  before_all do
+    # puts `pwd` 
+    # puts `echo $KUBECONFIG`
+    `crystal src/cnf-conformance.cr sample_coredns_cleanup`
+    $?.success?.should be_true
+  end
+  it "'wait_for_install' should wait for a cnf to be installed" do
+    current_dir = FileUtils.pwd 
+    puts current_dir
+    helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
+    puts helm
+    helm_install = `#{helm} install coredns stable/coredns`
+    puts helm_install
+    wait_for_install("coredns-coredns")
+    current_replicas = `kubectl get deployments coredns-coredns -o=jsonpath='{.status.readyReplicas}'`
+    (current_replicas.to_i > 0).should be_true
+  end
+end
