c14d336d3a61c542e78b5414418e2d7383bc5463
denverwilliams <denver@debian.nz>
2020-03-05 16:36:27 -0500

    #cncf/cnf-conformance/issues/80 helm_chart_valid now uses proper version of helm

--- src/tasks/cleanup.cr
+++ src/tasks/cleanup.cr
@@ -4,6 +4,6 @@ require "colorize"
 require "totem"
 
 desc "Cleans up the CNF Conformance test suite, the K8s cluster, and upstream projects"
-task "cleanup", ["sample_coredns_cleanup", "bad_helm_cnf_cleanup"] do  |_, args|
+task "cleanup", ["sample_coredns_cleanup", "bad_helm_cnf_cleanup", "helm_local_cleanup"] do  |_, args|
 end
 
