e19d7183e1853d7ad80780548a2067f5c7a0fb0e
denverwilliams <denver@debian.nz>
2020-03-05 14:08:48 -0500

    #cncf/cnf-conformance/issues/8 wait now back to 60

--- src/tasks/utils.cr
+++ src/tasks/utils.cr
@@ -24,7 +24,7 @@ def cnf_conformance_yml
   Totem.from_file "./#{cnf_conformance}"
 end
 
-def wait_for_install(deployment_name, wait_count=45)
+def wait_for_install(deployment_name, wait_count=60)
   second_count = 0
   current_replicas = `kubectl get deployments #{deployment_name} -o=jsonpath='{.status.readyReplicas}'`
   all_deployments = `kubectl get deployments`
