91d9998f215ef62857a5da37e156236a724503e6
denverwilliams <denver@debian.nz>
2020-03-10 14:16:46 -0400

    Add spec tests for privileged container 'whitelisted/non-whitelisted' cncf/cnf-conformance#55

--- spec/security_spec.cr
+++ spec/security_spec.cr
@@ -15,19 +15,27 @@ describe CnfConformance do
 
   end
   it "'privileged' should fail on a non-whitelisted, privileged cnf" do
-    # puts `pwd` 
-    # puts `echo $KUBECONFIG`
-    `crystal src/cnf-conformance.cr sample_coredns_cleanup`
-    $?.success?.should be_true
-    `crystal src/cnf-conformance.cr sample_privileged_cnf_setup`
-    $?.success?.should be_true
-    response_s = `crystal src/cnf-conformance.cr privileged verbose`
-    puts response_s
-    $?.success?.should be_true
-    (/Lint Failed/ =~ response_s).should_not be_nil
-    `crystal src/cnf-conformance.cr sample_privileged_cnf_cleanup`
-    $?.success?.should be_true
-    `crystal src/cnf-conformance.cr sample_coredns_setup`
-    $?.success?.should be_true
+    begin
+      `crystal src/cnf-conformance.cr sample_privileged_cnf_non_whitelisted_setup`
+      $?.success?.should be_true
+      response_s = `crystal src/cnf-conformance.cr privileged verbose`
+      puts response_s
+      $?.success?.should be_true
+      (/Found privileged containers.*coredns/ =~ response_s).should_not be_nil
+    ensure
+      `crystal src/cnf-conformance.cr sample_privileged_cnf_non_whitelisted_cleanup`
+    end
+  end
+  it "'privileged' should pass on a whitelisted, privileged cnf" do
+    begin
+      `crystal src/cnf-conformance.cr sample_privileged_cnf_whitelisted_setup`
+      $?.success?.should be_true
+      response_s = `crystal src/cnf-conformance.cr privileged verbose`
+      puts response_s
+      $?.success?.should be_true
+      (/Found privileged containers.*coredns/ =~ response_s).should be_nil
+    ensure
+      `crystal src/cnf-conformance.cr sample_privileged_cnf_whitelisted_cleanup`
+    end
   end
 end
