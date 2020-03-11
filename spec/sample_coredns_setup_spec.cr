b96725cb8d59045a2be3c2e75691ee3a237f9bd5
wolfpack <wolfpack@vulk.co>
2020-03-11 17:26:56 -0400

    #cncf/cnf-conformance/issues/56 sample_setup, sample_setup_args, and sample_cleanup now generically setup a sample cnf

--- spec/sample_coredns_setup_spec.cr
+++ spec/sample_coredns_setup_spec.cr
@@ -2,6 +2,7 @@ require "./spec_helper"
 require "colorize"
 require "../src/tasks/utils.cr"
 require "file_utils"
+require "sam"
 
 describe "Utils" do
   before_all do
@@ -9,10 +10,29 @@ describe "Utils" do
     # puts `echo $KUBECONFIG`
     `crystal src/cnf-conformance.cr helm_local_install`
     $?.success?.should be_true
-    `crystal src/cnf-conformance.cr sample_coredns_cleanup`
+  end
+
+  after_all do
+    # puts `pwd` 
+    # puts `echo $KUBECONFIG`
+    `crystal src/cnf-conformance.cr sample_coredns_setup`
     $?.success?.should be_true
   end
+
+  before_each do
+    `crystal src/cnf-conformance.cr cleanup`
+    $?.success?.should be_true
+  end
+
+  after_each do
+    `crystal src/cnf-conformance.cr cleanup`
+    $?.success?.should be_true
+  end
+  
   it "'wait_for_install' should wait for a cnf to be installed" do
+    `crystal src/cnf-conformance.cr sample_coredns_setup`
+    $?.success?.should be_true
+
     current_dir = FileUtils.pwd 
     puts current_dir
     helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
@@ -22,5 +42,36 @@ describe "Utils" do
     wait_for_install("coredns-coredns")
     current_replicas = `kubectl get deployments coredns-coredns -o=jsonpath='{.status.readyReplicas}'`
     (current_replicas.to_i > 0).should be_true
+  end
+
+  it "'sample_setup' should set up a sample cnf" do
+    args = Sam::Args.new
+    sample_setup(sample_dir: "sample-cnfs/sample-coredns-cnf", release_name: "coredns", deployment_name: "coredns-coredns", helm_chart: "stable/coredns", helm_directory: "cnfs/coredns/helm_chart/coredns", git_clone_url: "https://github.com/coredns/coredns.git" )
+    # check if directory exists
+    (Dir.exists? "sample-cnfs/sample-coredns-cnf").should be_true
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_true
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_true
+    (File.exists?("cnfs/coredns/helm_chart/coredns/Chart.yaml")).should be_true
+  end
+
+  it "'sample_setup_args' should set up a sample cnf from a argument", tags: "WIP" do
+    args = Sam::Args.new
+    sample_setup_args(sample_dir: "sample-cnfs/sample-coredns-cnf", args: args, verbose: true )
+    # check if directory exists
+    (Dir.exists? "sample-cnfs/sample-coredns-cnf").should be_true
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_true
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_true
+    (File.exists?("cnfs/coredns/helm_chart/coredns/Chart.yaml")).should be_true
+  end
+
+  it "'sample_cleanup' should clean up a sample cnf from a argument", tags: "WIP" do
+    args = Sam::Args.new
+    sample_setup_args(sample_dir: "sample-cnfs/sample-coredns-cnf", args: args, verbose: true )
+    sample_cleanup(sample_dir: "sample-cnfs/sample-coredns-cnf", verbose: true)
+    # check if directory exists
+    (Dir.exists? "cnfs/coredns").should be_false
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_false
+    (File.exists?("cnfs/coredns/cnf-conformance.yml")).should be_false
+    (File.exists?("cnfs/coredns/helm_chart/coredns/Chart.yaml")).should be_false
   end
 end
--- src/tasks/utils.cr
+++ src/tasks/utils.cr
@@ -1,3 +1,4 @@
+require "totem"
 # TODO make constants local or always retrieve from environment variables
 # TODO Move constants out
 CNF_DIR = "cnfs"
@@ -24,6 +25,14 @@ def cnf_conformance_yml
   Totem.from_file "./#{cnf_conformance}"
 end
 
+def sample_conformance_yml(sample_dir)
+  cnf_conformance = `find #{sample_dir}/* -name "cnf-conformance.yml"`.split("\n")[0]
+  if cnf_conformance.empty?
+    raise "No cnf_conformance.yml found in #{sample_dir}!"
+  end
+  Totem.from_file "./#{cnf_conformance}"
+end
+
 def wait_for_install(deployment_name, wait_count=180)
   second_count = 0
   current_replicas = `kubectl get deployments #{deployment_name} -o=jsonpath='{.status.readyReplicas}'`
@@ -38,3 +47,98 @@ def wait_for_install(deployment_name, wait_count=180)
     second_count = second_count + 1 
   end
 end 
+def sample_setup_args(sample_dir, args, deploy_with_chart=true, verbose=false)
+  # # Parse the cnf-conformance.yml
+  config = sample_conformance_yml(sample_dir)
+
+  if args.named.keys.includes? "release_name"
+    release_name = args.named["release_name"]
+  else
+    release_name = config.get("release_name").as_s 
+  end
+  puts "release_name: #{release_name}" if verbose
+
+  if args.named.keys.includes? "deployment_name"
+    deployment_name = args.named["deployment_name"]
+  else
+    deployment_name = config.get("deployment_name").as_s 
+  end
+  puts "deployment_name: #{deployment_name}" if verbose
+
+  if args.named.keys.includes? "helm_chart"
+    helm_chart = args.named["helm_chart"]
+  else
+    helm_chart = config.get("helm_chart").as_s 
+  end
+  puts "helm_chart: #{helm_chart}" if verbose
+
+  if args.named.keys.includes? "helm_directory"
+    helm_directory = args.named["helm_directory"]
+  else
+    helm_directory = config.get("helm_directory").as_s 
+  end
+  puts "helm_directory: #{helm_directory}" if verbose
+
+  if args.named.keys.includes? "git_clone_url"
+    git_clone_url = args.named["git_clone_url"]
+  else
+    git_clone_url = config.get("git_clone_url").as_s 
+  end
+  puts "git_clone_url: #{git_clone_url}" if verbose
+
+  sample_setup(sample_dir: sample_dir, release_name: release_name, deployment_name: deployment_name, helm_chart: helm_chart, helm_directory: helm_directory, git_clone_url: git_clone_url, deploy_with_chart: deploy_with_chart, verbose: verbose )
+
+end
+
+def sample_setup(sample_dir, release_name, deployment_name, helm_chart, helm_directory, git_clone_url, deploy_with_chart=true, verbose=false)
+
+  current_dir = FileUtils.pwd 
+  puts current_dir if verbose 
+
+  # TODO enable recloning/fetching etc
+  # TODO pass in block
+  git_clone = `git clone #{git_clone_url} #{current_dir}/#{CNF_DIR}/#{release_name}`    
+  puts git_clone if verbose
+
+  # Copy the cnf-conformance.yml
+  yml_cp = `cp #{sample_dir}/cnf-conformance.yml #{current_dir}/#{CNF_DIR}/#{release_name}`
+  puts yml_cp if verbose
+
+  begin
+
+    helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
+    puts helm if verbose
+    helm_install = `#{helm} install #{release_name} #{helm_chart}`
+    puts helm_install if verbose 
+
+
+    # Retrieve the helm chart source
+    FileUtils.mkdir_p("#{current_dir}/#{CNF_DIR}/#{release_name}/helm_chart") 
+    helm_pull = `#{helm} pull #{helm_chart}`
+    puts helm_pull if verbose 
+    core_mv = `mv #{release_name}-*.tgz #{current_dir}/#{CNF_DIR}/#{release_name}/helm_chart`
+    puts core_mv if verbose 
+    tar = `cd #{current_dir}/#{CNF_DIR}/#{release_name}/helm_chart; tar -xvf #{current_dir}/#{CNF_DIR}/#{release_name}/helm_chart/#{release_name}-*.tgz`
+    puts tar if verbose
+    wait_for_install(deployment_name)
+    if helm_install.to_s.size > 0 && helm_pull.to_s.size > 0
+      puts "Successfully setup #{release_name}".colorize(:green)
+    end
+  ensure
+    cd = `cd #{current_dir}`
+    puts cd if verbose 
+  end
+end
+
+def sample_cleanup(sample_dir, verbose=true)
+  config = sample_conformance_yml(sample_dir)
+  release_name = config.get("release_name").as_s 
+
+  current_dir = FileUtils.pwd 
+  helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
+  puts helm if verbose 
+  helm_uninstall = `#{helm} uninstall #{release_name}`
+  puts helm_uninstall if verbose
+  rm = `rm -rf #{current_dir}/#{CNF_DIR}/#{release_name}`
+  puts rm if verbose
+end
