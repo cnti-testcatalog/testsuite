fd875bc6c44ae7728fe4ab21e80c7dd8c1cb1c64
wolfpack <wolfpack@vulk.co>
2020-03-15 00:54:27 -0400

    #cncf/cnf-conformance/issues/56 bad helm sample cnf now generic

--- src/tasks/sample_coredns_setup.cr
+++ src/tasks/sample_coredns_setup.cr
@@ -16,74 +16,17 @@ task "sample_coredns_source_setup", ["helm_local_install"] do |_, args|
     sample_setup_args(sample_dir: "sample-cnfs/sample-coredns-cnf-source", args: args, verbose: true )
 end
 
+desc "Sets up an alternate sample CoreDNS CNF"
 task "sample_coredns", ["helm_local_install"] do |_, args|
   puts "sample_coredns new setup" if check_verbose(args)
-    sample_setup_args(sample_dir: "sample-cnfs/sample_coredns", deploy_with_chart: false, args: args, verbose: true )
+  sample_setup_args(sample_dir: "sample-cnfs/sample_coredns", deploy_with_chart: false, args: args, verbose: true )
 end
 
 
+desc "Sets up a Bad helm CNF Setup"
 task "bad_helm_cnf_setup", ["helm_local_install"] do |_, args|
-  current_dir = FileUtils.pwd 
-  puts current_dir if check_verbose(args)
-
-  # Retrieve the cnf source
-  # TODO enable recloning/fetching etc
-  git_clone = `git clone https://github.com/coredns/coredns.git #{current_dir}/#{CNF_DIR}/bad_helm_coredns`    
-  puts git_clone if check_verbose(args)
-
-  # Copy the cnf-conformance.yml
-  yml_cp = `cp sample-cnfs/sample-bad_helm_coredns-cnf/cnf-conformance.yml #{current_dir}/#{CNF_DIR}/bad_helm_coredns`
-  puts yml_cp if check_verbose(args)
-
-  # Parse the cnf-conformance.yml
-  config = cnf_conformance_yml
-
-  if args.named.keys.includes? "deployment_name"
-    deployment_name = args.named["deployment_name"]
-  else
-    deployment_name = config.get("deployment_name").as_s 
-  end
-  puts "deployment_name: #{deployment_name}" if check_verbose(args)
-
-  if args.named.keys.includes? "helm_chart"
-    helm_chart = args.named["helm_chart"]
-  else
-    helm_chart = config.get("helm_chart").as_s 
-  end
-  puts "helm_chart: #{helm_chart}" if check_verbose(args)
-
-
-  begin
-
-    helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
-    puts helm if check_verbose(args)
-    # name must be dns compliant (no underscores)
-    helm_install = `#{helm} install bad-helm-coredns #{helm_chart}`
-    puts helm_install if check_verbose(args)
-
-
-    # Retrieve the helm chart source
-    FileUtils.mkdir_p("#{current_dir}/#{CNF_DIR}/bad_helm_coredns/helm_chart") 
-    helm_pull = `#{helm} pull #{helm_chart}`
-    puts helm_pull if check_verbose(args)
-    core_mv = `mv coredns-*.tgz #{current_dir}/#{CNF_DIR}/bad_helm_coredns/helm_chart`
-    puts core_mv if check_verbose(args)
-    tar = `cd #{current_dir}/#{CNF_DIR}/bad_helm_coredns/helm_chart; tar -xvf #{current_dir}/#{CNF_DIR}/bad_helm_coredns/helm_chart/coredns-*.tgz`
-    puts tar if check_verbose(args)
-    
-    #sed command that changes helm chart to be bad
-    helm_sed = `sed -i s/"toYaml"/"sdfsdfsdf"/g #{current_dir}/#{CNF_DIR}/bad_helm_coredns/helm_chart/coredns/templates/deployment.yaml`
-    puts helm_sed if check_verbose(args)
-    
-    wait_for_install(deployment_name)
-
-    if helm_install.to_s.size > 0 && helm_pull.to_s.size > 0
-      puts "Successfully setup coredns".colorize(:green)
-    end
-  ensure
-    cd = `cd #{current_dir}`
-    puts cd if check_verbose(args)
-  end
+  puts "bad_helm_cnf_setup" if check_verbose(args)
+  sample_setup_args(sample_dir: "sample-cnfs/sample-bad_helm_coredns-cnf", deploy_with_chart: false, args: args, verbose: true, wait_count: 5 )
 end
 
 task "sample_privileged_cnf_whitelisted_setup", ["helm_local_install"] do |_, args|
@@ -206,13 +149,7 @@ end
 
 
 task "bad_helm_cnf_cleanup" do |_, args|
-  current_dir = FileUtils.pwd 
-  helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
-  puts helm if check_verbose(args)
-  helm_uninstall = `#{helm} uninstall bad-helm-coredns`
-  puts helm_uninstall if check_verbose(args)
-  rm = `rm -rf #{current_dir}/#{CNF_DIR}/bad_helm_coredns`
-  puts rm if check_verbose(args)
+  sample_cleanup(sample_dir: "sample-cnfs/sample-bad_helm_coredns-cnf", verbose: true)
 end
 
 task "sample_privileged_cnf_whitelisted_cleanup" do |_, args|
