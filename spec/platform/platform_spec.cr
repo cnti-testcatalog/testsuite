c90e601aca05bf3dcae4bccf5e994d0b50915ce1
denverwilliams <denver@debian.nz>
2020-03-25 18:41:12 -0400

    #cncf/cnf-conformance/issues/104 sonobuoy now has a before task

--- spec/platform/platform_spec.cr
+++ spec/platform/platform_spec.cr
@@ -2,6 +2,16 @@ require "./../spec_helper"
 require "colorize"
 
 describe "Platform" do
+  before_all do
+    # puts `pwd` 
+    # puts `echo $KUBECONFIG`
+    `crystal src/cnf-conformance.cr samples_cleanup`
+    $?.success?.should be_true
+    `crystal src/cnf-conformance.cr setup`
+    $?.success?.should be_true
+    `crystal src/cnf-conformance.cr sample_coredns_with_wait_setup`
+    $?.success?.should be_true
+  end
   it "'k8s_conformance' should pass if the sonobuoy tests pass" do
     response_s = `crystal src/cnf-conformance.cr k8s_conformance`
     puts response_s
