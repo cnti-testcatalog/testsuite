aa30db42035fb73d73dbc27125c20ea4b3e64ff7
wolfpack <wolfpack@vulk.co>
2020-04-29 12:52:23 -0400

    #cncf/cnf-conformance/issues/169 'all' task now tests for a published helm chart.

--- spec/cnf_conformance_spec.cr
+++ spec/cnf_conformance_spec.cr
@@ -32,22 +32,23 @@ describe CnfConformance do
     (/Lint Passed/ =~ response_s).should_not be_nil
     (/PASSED: Replicas increased to 3/ =~ response_s).should_not be_nil
     (/PASSED: Replicas decreased to 1/ =~ response_s).should_not be_nil
+    (/PASSED: Published Helm Chart Repo added/ =~ response_s).should_not be_nil
     (/Final score:/ =~ response_s).should_not be_nil
     
-    (all_result_test_names(final_cnf_results_yml)).should eq(["privileged", "increase_capacity", "decrease_capacity", "ip_addresses", "liveness", "readiness", "install_script_helm", "helm_chart_valid"])
+    (all_result_test_names(final_cnf_results_yml)).should eq(["privileged", "increase_capacity", "decrease_capacity", "ip_addresses", "liveness", "readiness", "install_script_helm", "helm_chart_valid", "helm_chart_published"])
   end
 
-  # it "'scalability' should run all of the scalability tests" do
-  #   # puts `pwd` 
-  #   # puts `echo $KUBECONFIG`
-  #   response_s = `crystal src/cnf-conformance.cr setup`
-  #   puts response_s
-  #   response_s = `crystal src/cnf-conformance.cr scalability`
-  #   puts response_s
-  #     $?.success?.should be_true
-  #   (/PASSED: Replicas increased to 3/ =~ response_s).should_not be_nil
-  #   (/PASSED: Replicas decreased to 1/ =~ response_s).should_not be_nil
-  # end
+  it "'scalability' should run all of the scalability tests" do
+    # puts `pwd` 
+    # puts `echo $KUBECONFIG`
+    response_s = `crystal src/cnf-conformance.cr setup`
+    puts response_s
+    response_s = `crystal src/cnf-conformance.cr scalability`
+    puts response_s
+      $?.success?.should be_true
+    (/PASSED: Replicas increased to 3/ =~ response_s).should_not be_nil
+    (/PASSED: Replicas decreased to 1/ =~ response_s).should_not be_nil
+  end
 
 
 end
