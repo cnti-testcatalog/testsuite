512a141092e4f9bfb503040c3edbdee929b12451
denverwilliams <denver@debian.nz>
2020-05-07 15:00:44 -0400

    Fix spec for published helm chart test cncf/cnf-conformance#169

--- spec/cnf_conformance_spec.cr
+++ spec/cnf_conformance_spec.cr
@@ -36,7 +36,7 @@ describe CnfConformance do
     (/Lint Passed/ =~ response_s).should_not be_nil
     (/PASSED: Replicas increased to 3/ =~ response_s).should_not be_nil
     (/PASSED: Replicas decreased to 1/ =~ response_s).should_not be_nil
-    (/PASSED: Published Helm Chart Repo added/ =~ response_s).should_not be_nil
+    (/PASSED: Published Helm Chart Found/ =~ response_s).should_not be_nil
     (/Final score:/ =~ response_s).should_not be_nil
     (all_result_test_names(final_cnf_results_yml)).should eq(["privileged", "increase_capacity", "decrease_capacity", "ip_addresses", "liveness", "readiness", "install_script_helm", "helm_chart_valid", "helm_chart_published", "reasonable_image_size", "reasonable_startup_time"])
   end
