190cfaf419bb095f553078c84a0b3ad9edbdbc88
wolfpack <wolfpack@vulk.co>
2020-05-20 07:14:46 -0400

    #cnf-conformance/issues/213 cleanup now forced in microservices

--- spec/microservice_spec.cr
+++ spec/microservice_spec.cr
@@ -17,32 +17,39 @@ describe "Microservice" do
   end
 
   it "'reasonable_startup_time' should pass if the cnf has a reasonable startup time(helm_directory)", tags: ["reasonable_startup_time", "happy-path"]  do
-    `./cnf-conformance sample_coredns_cleanup`
-    $?.success?.should be_true
-    response_s = `./cnf-conformance reasonable_startup_time yml-file=sample-cnfs/sample_coredns/cnf-conformance.yml`
-    $?.success?.should be_true
-    (/PASSED: CNF had a reasonable startup time/ =~ response_s).should_not be_nil
-    `./cnf-conformance sample_coredns_cleanup`
+    begin
+      `./cnf-conformance sample_coredns_cleanup`
+      $?.success?.should be_true
+      response_s = `./cnf-conformance reasonable_startup_time yml-file=sample-cnfs/sample_coredns/cnf-conformance.yml`
+      $?.success?.should be_true
+      (/PASSED: CNF had a reasonable startup time/ =~ response_s).should_not be_nil
+    ensure
+      `./cnf-conformance sample_coredns_cleanup force=true`
+    end
   end
 
   it "'reasonable_startup_time' should fail if the cnf doesn't has a reasonable startup time(helm_directory)", tags: "reasonable_startup_time" do
-    `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_envoy_slow_startup force=true`
-    $?.success?.should be_true
-    response_s = `./cnf-conformance reasonable_startup_time yml-file=sample-cnfs/sample_envoy_slow_startup/cnf-conformance.yml`
-    $?.success?.should be_true
-    (/FAILURE: CNF had a startup time of/ =~ response_s).should_not be_nil
-    `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_envoy_slow_startup force=true`
+    begin
+      `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_envoy_slow_startup force=true`
+      $?.success?.should be_true
+      response_s = `./cnf-conformance reasonable_startup_time yml-file=sample-cnfs/sample_envoy_slow_startup/cnf-conformance.yml`
+      $?.success?.should be_true
+      (/FAILURE: CNF had a startup time of/ =~ response_s).should_not be_nil
+    ensure
+      `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_envoy_slow_startup force=true`
+    end
   end
 
   it "'reasonable_image_size' should pass if image is smaller than 5gb", tags: ["reasonable_image_size","happy-path"]  do
     begin
+      `./cnf-conformance cleanup force=true`
       `./cnf-conformance sample_coredns_setup`
       response_s = `./cnf-conformance reasonable_image_size verbose`
       puts response_s
       $?.success?.should be_true
       (/Image size is good/ =~ response_s).should_not be_nil
     ensure
-      `./cnf-conformance sample_coredns_cleanup`
+      `./cnf-conformance sample_coredns_cleanup force=true`
     end
   end
 
