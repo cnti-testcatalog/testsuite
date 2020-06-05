be8493ba88b820e2431a74e569a4c0b6e55f8076
denverwilliams <denver@debian.nz>
2020-06-05 15:41:45 -0400

    Add test for CPU Hog Chaos test cncf/cnf-conformance#231

--- src/tasks/resilience.cr
+++ src/tasks/resilience.cr
@@ -27,6 +27,7 @@ task "install_chaosmesh" do |_, args|
   end
   install_chaos_mesh = `#{helm} install chaos-mesh #{current_dir}/#{TOOLS_DIR}/chaos_mesh/helm/chaos-mesh --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock`
   wait_for_resource("#{current_dir}/spec/fixtures/chaos_network_loss.yml")
+  wait_for_resource("#{current_dir}/spec/fixtures/chaos_cpu_hog.yml")
 end
 
 desc "Does the CNF crash when network loss occurs"
@@ -50,13 +51,13 @@ task "chaos_network_loss", ["install_chaosmesh", "retrieve_manifest"] do |_, arg
       puts ex.message 
     end
     if errors < 1
-      template = Crinja.render(chaos_template, { "deployment_label" => "#{deployment_label}", "deployment_label_value" => "#{deployment_label_value}" })
+      template = Crinja.render(network_chaos_template, { "deployment_label" => "#{deployment_label}", "deployment_label_value" => "#{deployment_label_value}" })
       chaos_config = `echo "#{template}" > "#{destination_cnf_dir}/chaos_network_loss.yml"`
       puts "#{chaos_config}" if check_verbose(args)
       run_chaos = `kubectl create -f "#{destination_cnf_dir}/chaos_network_loss.yml"`
       puts "#{run_chaos}" if check_verbose(args)
       # TODO fail if exceeds
-      if wait_for_test("network-loss")
+      if wait_for_test("NetworkChaos", "network-loss")
         LOGGING.info( "Wait Done")
         if desired_is_available?(deployment_name)
           resp = upsert_passed_task("chaos_network_loss","✔️  PASSED: Replicas available match desired count after network chaos test #{emoji_chaos_network_loss}")
@@ -76,14 +77,60 @@ task "chaos_network_loss", ["install_chaosmesh", "retrieve_manifest"] do |_, arg
   end
 end
 
-def wait_for_test(test_name)
+desc "Does the CNF crash when CPU usage is high"
+task "chaos_cpu_hog", ["install_chaosmesh", "retrieve_manifest"] do |_, args|
+  task_response = task_runner(args) do |args|
+    config = parsed_config_file(ensure_cnf_conformance_yml_path(args.named["cnf-config"].as(String)))
+    destination_cnf_dir = cnf_destination_dir(ensure_cnf_conformance_dir(args.named["cnf-config"].as(String)))
+    deployment_name = config.get("deployment_name").as_s
+    deployment_label = config.get("deployment_label").as_s
+    helm_chart_container_name = config.get("helm_chart_container_name").as_s
+    puts "#{destination_cnf_dir}"
+    LOGGING.info "destination_cnf_dir #{destination_cnf_dir}"
+    deployment = Totem.from_file "#{destination_cnf_dir}/manifest.yml"
+
+    errors = 0
+    begin
+      deployment_label_value = deployment.get("metadata").as_h["labels"].as_h[deployment_label].as_s
+    rescue ex
+      errors = errors + 1
+      puts ex.message 
+    end
+    if errors < 1
+      template = Crinja.render(cpu_chaos_template, { "deployment_label" => "#{deployment_label}", "deployment_label_value" => "#{deployment_label_value}" })
+      chaos_config = `echo "#{template}" > "#{destination_cnf_dir}/chaos_cpu_hog.yml"`
+      puts "#{chaos_config}" if check_verbose(args)
+      run_chaos = `kubectl create -f "#{destination_cnf_dir}/chaos_cpu_hog.yml"`
+      puts "#{run_chaos}" if check_verbose(args)
+      # TODO fail if exceeds
+      if wait_for_test("StressChaos", "burn-cpu")
+        if desired_is_available?(deployment_name)
+          resp = upsert_passed_task("chaos_network_loss","✔️  PASSED: Replicas available match desired count after cpu chaos test")
+        else
+          resp = upsert_failed_task("chaos_network_loss","✖️  FAILURE: Replicas did not return desired count after cpu chaos test")
+        end
+      else
+        # TODO Change this to an exception (points = 0)
+        # e.g. upsert_exception_task
+        resp = upsert_failed_task("chaos_network_loss","✖️  FAILURE: Chaosmesh failed to finish.")
+      end
+      delete_chaos = `kubectl delete -f "#{destination_cnf_dir}/chaos_cpu_hog.yml"`
+    else
+      resp = upsert_failed_task("chaos_network_loss","✖️  FAILURE: No deployment label found for network chaos test")
+    end
+    delete_chaos_mesh
+  end
+end
+
+
+def wait_for_test(test_type, test_name)
   second_count = 0
   wait_count = 60
   status = ""
   until (status.empty? != true && status == "Finished") || second_count > wait_count.to_i
     puts "second_count = #{second_count}"
     sleep 1
-    get_status = `kubectl get NetworkChaos "#{test_name}" -o yaml`
+    get_status = `kubectl get "#{test_type}" "#{test_name}" -o yaml`
     LOGGING.info("#{get_status}")
     status_data = Totem.from_yaml("#{get_status}")
     puts "Status: #{get_status}"
@@ -131,7 +178,7 @@ def wait_for_resource(resource_file)
   `kubectl delete -f #{resource_file}`
 end
 
-def chaos_template
+def network_chaos_template
   <<-TEMPLATE
   apiVersion: pingcap.com/v1alpha1
   kind: NetworkChaos
@@ -153,8 +200,32 @@ def chaos_template
   TEMPLATE
 end
 
+def cpu_chaos_template
+  <<-TEMPLATE
+  apiVersion: pingcap.com/v1alpha1
+  kind: StressChaos
+  metadata:
+    name: burn-cpu
+    namespace: default
+  spec:
+    mode: one
+    selector:
+      labelSelectors:
+        '{{ deployment_label}}': '{{ deployment_label_value }}'
+    stressors:
+      cpu:
+        workers: 1
+        load: 100
+        options: ['-c 0']
+    duration: '40s'
+    scheduler:
+      cron: '@every 600s'
+  TEMPLATE
+end
+
+
 def delete_chaos_mesh
-  current_dir = FileUtils.pwd 
+  current_dir = FileUtils.pwd
   helm = "#{current_dir}/#{TOOLS_DIR}/helm/linux-amd64/helm"
   crd_delete = `kubectl delete -f https://raw.githubusercontent.com/pingcap/chaos-mesh/master/manifests/crd.yaml`
   FileUtils.rm_rf("#{current_dir}/#{TOOLS_DIR}/chaos_mesh")
