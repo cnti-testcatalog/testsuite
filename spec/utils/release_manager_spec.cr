5ce85d2adcdb0c0af2694376e809f7794d91e237
denverwilliams <denver@debian.nz>
2020-07-08 14:05:53 -0400

    cncf/cnf-conformance#284 releases for tagged version now a prerelease, but not master

---
+++ spec/utils/release_manager_spec.cr
@@ -0,0 +1,60 @@
+require "../spec_helper"
+require "colorize"
+require "../../src/tasks/utils/utils.cr"
+require "../../src/tasks/utils/release_manager.cr"
+require "file_utils"
+require "sam"
+
+describe "ReleaseManager" do
+  after_all do
+    ReleaseManager::GithubReleaseManager.delete_release("test_version")
+    $?.success?.should be_true
+  end
+  it "'#ReleaseManager.tag' should return the list of tags on the current branch"  do
+    (ReleaseManager.tag).should contain("v0.3.0")
+  end
+  it "'#ReleaseManager.tag' should accept a list of options"  do
+      (ReleaseManager.tag("--list")).should_not eq([""])
+      (ReleaseManager.tag("--list 'shouldbeempty'")).should eq([""]) 
+  end
+  it "'#ReleaseManager.current_branch' should return the current branch as a string"  do
+    if ReleaseManager.on_a_tag?
+      (ReleaseManager.current_branch).should eq("HEAD")
+    else
+      (ReleaseManager.current_branch).should_not eq("")
+    end
+  end
+
+  it "'#ReleaseManager.current_hash' should return the current hash as a string"  do
+    (ReleaseManager.current_hash).should_not eq("")
+  end
+
+  it "'#ReleaseManager::GithubReleaseManager.github_releases' should return the existing releases", tags: "release"  do
+    (ReleaseManager::GithubReleaseManager.github_releases.size).should be > 0
+  end
+
+  it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return the upserted release and asset response", tags: "release"  do
+    found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("test_version")
+    # (asset["errors"]?==nil || (asset["errors"]? && asset["errors"].find |x| {x["code"] =~ /already_exists/})).should be_truthy
+    if asset
+      (asset["errors"]?==nil || (asset["errors"]? && asset["errors"][0]["code"]  == "already_exists")).should be_truthy
+    else
+      (asset).should_not be_nil
+    end
+    # (asset["url"]?).should match("https://api.github.com/repos/cncf/cnf-conformance/releases/assets")
+
+  end
+
+  # TODO uncomment after travis is tested and working
+  # it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return nil if not on a valid version", tags: "release"  do
+  #   found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("invalid_version")
+  #   (asset).should be_nil
+  # end
+
+  it "'#ReleaseManager::GithubReleaseManager.delete_release' should delete the release from the found_id", tags: "release"  do
+    resp_code = ReleaseManager::GithubReleaseManager.delete_release("test_version")
+    (resp_code == 204).should be_truthy
+
+  end
+
+end
---
+++ src/tasks/utils/release_manager.cr
@@ -0,0 +1,139 @@
+require "totem"
+require "colorize"
+require "./sample_utils.cr"
+require "logger"
+require "halite"
+
+module ReleaseManager 
+  module GithubReleaseManager
+    def self.github_releases : Array(JSON::Any)
+      existing_releases = Halite.basic_auth(user: ENV["GITHUB_USER"], pass: ENV["GITHUB_TOKEN"]).
+        get("https://api.github.com/repos/cncf/cnf-conformance/releases", 
+            headers: {Accept: "application/vnd.github.v3+json"})
+      JSON.parse(existing_releases.body).as_a
+    end 
+
+    def self.upsert_release(version=nil) : Tuple((JSON::Any | Nil), (JSON::Any | Nil))
+      LOGGING.info "upsert_release"
+      found_release : (JSON::Any | Nil) = nil
+      asset : (JSON::Any | Nil) = nil
+      upsert_version = (version || CnfConformance::VERSION)
+      if upsert_version =~ /(?i)(master)/
+        prerelease = false
+      else
+        prerelease = true
+      end
+      # TODO uncomment after travis is tested and working
+      # unless upsert_version =~ /(?i)(master|v[0-1]|test_version)/
+      #   LOGGING.info "Not creating a release for : #{upsert_version}"
+      #   return {found_release, asset} 
+      # end
+      LOGGING.info "upsert_version: #{upsert_version}"
+      release_resp = ReleaseManager::GithubReleaseManager.github_releases
+      LOGGING.info "release_resp size: #{release_resp.size}"
+      found_release = release_resp.find {|x| x["tag_name"] == upsert_version} 
+      LOGGING.info "find found_release?: #{found_release}"
+      unless found_release
+        # /repos/:owner/:repo/releases
+        found_resp = Halite.basic_auth(user: ENV["GITHUB_USER"], pass: ENV["GITHUB_TOKEN"]).
+          post("https://api.github.com/repos/cncf/cnf-conformance/releases", 
+               headers: {Accept: "application/vnd.github.v3+json"},
+               json: { "tag_name" => upsert_version, 
+                       "draft" => true, 
+                       "prerelease" => prerelease, 
+                       "name" => "#{upsert_version} #{Time.local.to_s("%B, %d %Y")}", 
+                       "body" => "TEMPLATE: Fill out release notes" })
+        found_release = JSON.parse(found_resp.body)
+        # TODO error if cant create a release
+        LOGGING.info "(unless) found_release: #{found_release}"
+      end
+      cnf_tarball_name = "cnf-conformance-#{upsert_version}.tar.gz"
+      cnf_tarball = `tar -czvf #{cnf_tarball_name} ./cnf-conformance`
+      LOGGING.info "cnf_tarball: #{cnf_tarball}"
+
+      # PATCH /repos/:owner/:repo/releases/:release_id
+      found_resp = Halite.basic_auth(user: ENV["GITHUB_USER"], pass: ENV["GITHUB_TOKEN"]).
+        patch("https://api.github.com/repos/cncf/cnf-conformance/releases/#{found_release["id"]}", 
+              json: { "tag_name" => upsert_version,
+                      "draft" => true, 
+                      "prerelease" => prerelease, 
+                      "name" => "#{upsert_version} #{Time.local.to_s("%B, %d %Y")}", 
+                      "body" => "TEMPLATE: Fill out release notes" })
+      found_release = JSON.parse(found_resp.body)
+
+      LOGGING.info "found_release (after create): #{found_release}"
+      LOGGING.info "found_release id: #{found_release["id"]}"
+      # POST :server/repos/:owner/:repo/releases/:release_id/assets{?name,label}
+      asset_resp = Halite.basic_auth(user: ENV["GITHUB_USER"], pass: ENV["GITHUB_TOKEN"]).
+        post("https://uploads.github.com/repos/cncf/cnf-conformance/releases/#{found_release["id"]}/assets?name=#{cnf_tarball_name}",
+             headers: {
+                "Content-Type" => "application/gzip",
+                "Content-Length" => File.size("#{cnf_tarball_name}").to_s 
+        }, raw: "#{File.open("#{cnf_tarball_name}")}")
+        # asset = `curl -u nupejosh:#{ENV["GITHUB_TOKEN"]} -H "Content-Type: $(file -b --mime-type #{cnf_filename})" --data-binary @#{cnf_filename} "https://uploads.github.com/repos/cncf/cnf-conformance/releases/#{found_release["id"]}/assets?name=$(basename #{cnf_filename})"`
+       asset = JSON.parse(asset_resp.body)
+      LOGGING.info "asset: #{asset}"
+      {found_release, asset}
+    end
+
+    def self.delete_release(version)
+      # DELETE /repos/:owner/:repo/releases/assets/:asset_id
+      # DELETE /repos/:owner/:repo/releases/:release_id
+      release_resp = ReleaseManager::GithubReleaseManager.github_releases
+      puts "this is the version #{version}"
+      found_release = release_resp.find {|x| x["tag_name"] == "#{version}"} 
+      puts "this is found_release #{typeof(found_release)}"
+      if found_release
+        puts "this is found_release id #{found_release["id"]}"
+        resp = Halite.basic_auth(user: ENV["GITHUB_USER"], pass: ENV["GITHUB_TOKEN"]).
+          delete("https://api.github.com/repos/cncf/cnf-conformance/releases/#{found_release["id"]}")
+        resp_code = resp.status_code
+        LOGGING.info "resp_code: #{resp_code}"
+      else 
+        resp_code = 404
+      end 
+      resp_code
+    end 
+
+    #TODO get github secrets and add them to the CI (e.g. travis) secrets list 
+  end
+  module CompileTimeVersionGenerater
+    macro tagged_version
+      {% current_branch = `git rev-parse --abbrev-ref HEAD` %}
+      {% current_hash = `git rev-parse --short HEAD` %}
+      {% current_tag = `git tag --points-at HEAD` %}
+      {% if current_tag.strip == "" %}
+        VERSION = "{{current_branch.strip}}-{{current_hash.strip}}"
+      {% else %}
+        VERSION = "{{current_tag.strip}}"
+      {% end %}
+    end
+  end
+
+  def self.tag(options="")
+    results = `git tag #{options}`
+    LOGGING.info "git tag: #{results}"
+    results.split("\n")
+  end
+
+  def self.on_a_tag?
+    ReleaseManager.tag("--points-at HEAD") != [""]
+  end
+
+  def self.current_tag
+    ReleaseManager.tag("--points-at HEAD")
+  end
+
+  def self.current_branch
+    results = `git rev-parse --abbrev-ref HEAD`
+    LOGGING.info "current_branch rev-parse: #{results}"
+    results.strip("\n")
+  end
+
+  def self.current_hash
+    results = `git rev-parse --short HEAD`
+    LOGGING.info "current_hash rev-parse: #{results}"
+    results.strip("\n")
+  end
+
+end 
