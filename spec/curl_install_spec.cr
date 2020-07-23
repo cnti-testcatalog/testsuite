c7f81f2bfbc9ad6f6dab3da360960ac35be1e931
denverwilliams <denver@debian.nz>
2020-07-23 15:38:52 -0400

    #301 curl install now has specs

---
+++ spec/curl_install_spec.cr
@@ -0,0 +1,29 @@
+# coding: utf-8
+require "./spec_helper"
+require "colorize"
+require "../src/tasks/utils/utils.cr"
+require "file_utils"
+require "sam"
+
+describe "CurlInstall" do
+  after_all do
+    LOGGING.info "Curl install tests finished.  Building ./cnf-conformance again".colorize(:green)
+    `crystal build src/cnf-conformance.cr`
+    if $?.success?
+      LOGGING.info "Build Success!".colorize(:green)
+    else
+      LOGGING.info "crystal build failed!".colorize(:red)
+      raise "crystal build failed in spec_helper"
+    end
+  end
+  it "'source curl_install.sh)' should download a cnf-conformance binary"  do
+    response_s = `/bin/bash -c "source ./curl_install.sh"`
+    LOGGING.info response_s
+    (/cnf-conformance/ =~ response_s).should_not be_nil
+  end
+  it "'curl_install.sh' should download a cnf-conformance binary"  do
+    response_s = `./curl_install.sh`
+    LOGGING.info response_s
+    (/To use cnf-conformance please restart you terminal session to load the new Path/ =~ response_s).should_not be_nil
+  end
+end
