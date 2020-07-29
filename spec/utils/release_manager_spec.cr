74a757e6873b7c5f31686f59907b078b939ade96
W. Watson <w.watson@vulk.coop>
2020-07-29 00:38:16 -0400

    #306 testing origin/master

--- spec/utils/release_manager_spec.cr
+++ spec/utils/release_manager_spec.cr
@@ -9,52 +9,52 @@ describe "ReleaseManager" do
   after_all do
     ReleaseManager::GithubReleaseManager.delete_release("test_version")
   end
-  it "'#ReleaseManager.tag' should return the list of tags on the current branch"  do
-    (ReleaseManager.tag.size).should be > 0
-  end
-  it "'#ReleaseManager.tag' should accept a list of options"  do
-      (ReleaseManager.tag("--list")).should_not eq([""])
-      (ReleaseManager.tag("--list 'shouldbeempty'")).should eq([""]) 
-  end
-  it "'#ReleaseManager.current_branch' should return the current branch as a string"  do
-    if ReleaseManager.on_a_tag?
-      (ReleaseManager.tag("--list")).should_not eq([""])
-    else
-      (ReleaseManager.current_branch).should_not eq("")
-    end
-  end
-
-  it "'#ReleaseManager.current_hash' should return the current hash as a string"  do
-    (ReleaseManager.current_hash).should_not eq("")
-  end
-
-  it "'#ReleaseManager.remote_master_branch_hash' should return the current hash as a string"  do
-    (ReleaseManager.remote_master_branch_hash).should_not eq("")
-  end
-
-  it "'#ReleaseManager::GithubReleaseManager.github_releases' should return the existing releases", tags: "release"  do
-    (ReleaseManager::GithubReleaseManager.github_releases.size).should be > 0
-  end
-
-  it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return the upserted release and asset response", tags: "release"  do
-    found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("test_version")
-    if asset
-      (asset["errors"]?==nil || (asset["errors"]? && asset["errors"][0]["code"]  == "already_exists")).should be_truthy
-    else
-      (asset).should_not be_nil
-    end
-  end
-
-  it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return nil if not on a valid version", tags: "release"  do
-    found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("invalid_version")
-    (asset).should be_nil
-  end
-
-  it "'#ReleaseManager::GithubReleaseManager.delete_release' should delete the release from the found_id", tags: "release"  do
-    found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("test_version")
-    resp_code = ReleaseManager::GithubReleaseManager.delete_release("test_version")
-    (resp_code == 204).should be_truthy
-  end
+  # it "'#ReleaseManager.tag' should return the list of tags on the current branch"  do
+  #   (ReleaseManager.tag.size).should be > 0
+  # end
+  # it "'#ReleaseManager.tag' should accept a list of options"  do
+  #     (ReleaseManager.tag("--list")).should_not eq([""])
+  #     (ReleaseManager.tag("--list 'shouldbeempty'")).should eq([""]) 
+  # end
+  # it "'#ReleaseManager.current_branch' should return the current branch as a string"  do
+  #   if ReleaseManager.on_a_tag?
+  #     (ReleaseManager.tag("--list")).should_not eq([""])
+  #   else
+  #     (ReleaseManager.current_branch).should_not eq("")
+  #   end
+  # end
+  #
+  # it "'#ReleaseManager.current_hash' should return the current hash as a string"  do
+  #   (ReleaseManager.current_hash).should_not eq("")
+  # end
+  #
+  # it "'#ReleaseManager.remote_master_branch_hash' should return the current hash as a string"  do
+  #   (ReleaseManager.remote_master_branch_hash).should_not eq("")
+  # end
+  #
+  # it "'#ReleaseManager::GithubReleaseManager.github_releases' should return the existing releases", tags: "release"  do
+  #   (ReleaseManager::GithubReleaseManager.github_releases.size).should be > 0
+  # end
+  #
+  # it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return the upserted release and asset response", tags: "release"  do
+  #   found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("test_version")
+  #   if asset
+  #     (asset["errors"]?==nil || (asset["errors"]? && asset["errors"][0]["code"]  == "already_exists")).should be_truthy
+  #   else
+  #     (asset).should_not be_nil
+  #   end
+  # end
+  #
+  # it "'#ReleaseManager::GithubReleaseManager.upsert_release' should return nil if not on a valid version", tags: "release"  do
+  #   found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("invalid_version")
+  #   (asset).should be_nil
+  # end
+  #
+  # it "'#ReleaseManager::GithubReleaseManager.delete_release' should delete the release from the found_id", tags: "release"  do
+  #   found_release, asset = ReleaseManager::GithubReleaseManager.upsert_release("test_version")
+  #   resp_code = ReleaseManager::GithubReleaseManager.delete_release("test_version")
+  #   (resp_code == 204).should be_truthy
+  # end
 
   it "'#ReleaseManager.commit_message_issues' should list previsions releases", tags: "release"  do
     issues = ReleaseManager.commit_message_issues("0.0.0", "origin/master")
@@ -67,9 +67,9 @@ describe "ReleaseManager" do
     (issues.match(/^(.|)(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/)).should_not be_nil
   end
 
-  it "'#ReleaseManager.issue_title' should return issue title", tags: "release"  do
-    issues = ReleaseManager.issue_title("#318")
-    (issues.match(/#206 documentation update/)).should_not be_nil
-  end
+  # it "'#ReleaseManager.issue_title' should return issue title", tags: "release"  do
+  #   issues = ReleaseManager.issue_title("#318")
+  #   (issues.match(/#206 documentation update/)).should_not be_nil
+  # end
 
 end
