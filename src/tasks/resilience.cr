979a753058a4417eaa76d9a88231309a5dace9ba
W. Watson <w.watson@vulk.coop>
2020-07-30 15:57:26 -0400

    #322 moving functions back into resilience.cr

--- src/tasks/resilience.cr
+++ src/tasks/resilience.cr
@@ -153,6 +153,61 @@ task "chaos_container_kill", ["install_chaosmesh", "retrieve_manifest"] do |_, a
   end
 end
 
+def wait_for_test(test_type, test_name)
+  second_count = 0
+  wait_count = 60
+  status = ""
+  until (status.empty? != true && status == "Finished") || second_count > wait_count.to_i
+    LOGGING.debug "second_count = #{second_count}"
+    sleep 1
+    get_status = `kubectl get "#{test_type}" "#{test_name}" -o yaml`
+    LOGGING.info("#{get_status}")
+    status_data = Totem.from_yaml("#{get_status}")
+    LOGGING.info "Status: #{get_status}"
+    LOGGING.debug("#{status_data}")
+    status = status_data.get("status").as_h["experiment"].as_h["phase"].as_s
+    second_count = second_count + 1
+    LOGGING.info "#{get_status}"
+    LOGGING.info "#{second_count}"
+  end
+  # Did chaos mesh finish the test successfully
+  (status.empty? !=true && status == "Finished")
+end
+
+def desired_is_available?(deployment_name)
+  resp = `kubectl get deployments #{deployment_name} -o=yaml`
+  describe = Totem.from_yaml(resp)
+  LOGGING.info("desired_is_available describe: #{describe.inspect}")
+  desired_replicas = describe.get("status").as_h["replicas"].as_i
+  LOGGING.info("desired_is_available desired_replicas: #{desired_replicas}")
+  ready_replicas = describe.get("status").as_h["readyReplicas"]?
+  unless ready_replicas.nil?
+    ready_replicas = ready_replicas.as_i
+  else
+    ready_replicas = 0
+  end
+  LOGGING.info("desired_is_available ready_replicas: #{ready_replicas}")
+
+  desired_replicas == ready_replicas
+end
+
+def wait_for_resource(resource_file)
+  second_count = 0
+  wait_count = 60
+  is_resource_created = nil
+  until (is_resource_created.nil? != true && is_resource_created == true) || second_count > wait_count.to_i
+    LOGGING.info "second_count = #{second_count}"
+    sleep 3
+    `kubectl create -f #{resource_file} 2>&1 >/dev/null`
+    is_resource_created = $?.success?
+    LOGGING.info "Waiting for CRD"
+    LOGGING.info "Status: #{is_resource_created}"
+    LOGGING.debug "resource file: #{resource_file}"
+    second_count = second_count + 1
+  end
+  `kubectl delete -f #{resource_file}`
+end
+
 def network_chaos_template
   <<-TEMPLATE
   apiVersion: pingcap.com/v1alpha1
