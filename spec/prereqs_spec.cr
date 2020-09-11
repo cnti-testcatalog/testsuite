80a96f81716923d1ec47339e3074d53fbf386013
wwatson <w.watson@vulk.coop>
2020-09-11 14:14:25 -0400

    #385 git now part of prereqs

--- spec/prereqs_spec.cr
+++ spec/prereqs_spec.cr
@@ -6,7 +6,7 @@ require "file_utils"
 require "sam"
 
 describe "Prereq" do
-  it "'prereq' should check the system for prerequisites", tags: "happy-path"  do
+  it "'prereq' should check the system for prerequisites", tags: "prereqs"  do
     response_s = `./cnf-conformance prereqs verbose`
     LOGGING.info response_s
     $?.success?.should be_true
@@ -14,6 +14,7 @@ describe "Prereq" do
     (/wget found/ =~ response_s).should_not be_nil
     (/curl found/ =~ response_s).should_not be_nil
     (/kubectl found/ =~ response_s).should_not be_nil
+    (/git found/ =~ response_s).should_not be_nil
   end
 
 end
--- src/tasks/prereqs.cr
+++ src/tasks/prereqs.cr
@@ -6,12 +6,14 @@ require "./utils/system_information/helm.cr"
 require "./utils/system_information/wget.cr"
 require "./utils/system_information/curl.cr"
 require "./utils/system_information/kubectl.cr"
+require "./utils/system_information/git.cr"
 
 task "prereqs" do  |_, args|
   if helm_installation.includes?("helm found") &&
       wget_installation.includes?("wget found") &&
       curl_installation.includes?("curl found") &&
-      kubectl_installation.includes?("kubectl found")
+      kubectl_installation.includes?("kubectl found") &&
+      git_installation.includes?("git found")
       stdout_success "All prerequisites found."
   else
     stdout_failure "Setup failed. Some prerequisites are missing. Please install all of the prerequisites before continuing."
