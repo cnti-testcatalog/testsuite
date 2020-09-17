9e4521a5e63135047d2bb9db05d8d6fb1a698573
denverwilliams <denver@debian.nz>
2020-09-17 11:59:04 -0400

    Add Observability Check for the Prometheus NodeExporter cncf/cnf-conformance#343

--- spec/platform/observability_spec.cr
+++ spec/platform/observability_spec.cr
@@ -15,8 +15,13 @@ describe "Observability" do
   it "'kube_state_metrics' should return some json", tags: "platform:kube_state_metrics" do
       response_s = `./cnf-conformance platform:kube_state_metrics poc`
       LOGGING.info response_s
-      # (/(PASSED){1}.*(which are OCI compliant runtimes){1}/ =~ response_s).should_not be_nil
       (/(PASSED){1}.*(Your platform is using the){1}.*(release for kube state metrics){1}/ =~ response_s).should_not be_nil
+  end
+
+  it "'node_exporter' should return some json", tags: "platform:node_exporter" do
+      response_s = `./cnf-conformance platform:node_exporter poc`
+      LOGGING.info response_s
+      (/(PASSED){1}.*(Your platform is using the){1}.*(release for the node exporter){1}/ =~ response_s).should_not be_nil
   end
 
 end
--- src/tasks/platform/observability.cr
+++ src/tasks/platform/observability.cr
@@ -5,7 +5,7 @@ require "../utils/utils.cr"
 
 namespace "platform" do
   desc "The CNF conformance suite checks to see if the Platform has Observability support."
-  task "observability", ["kube_state_metrics"] do |t, args|
+  task "observability", ["kube_state_metrics", "node_exporter"] do |t, args|
     VERBOSE_LOGGING.info "resilience" if check_verbose(args)
     VERBOSE_LOGGING.debug "resilience args.raw: #{args.raw}" if check_verbose(args)
     VERBOSE_LOGGING.debug "resilience args.named: #{args.named}" if check_verbose(args)
@@ -26,15 +26,14 @@ namespace "platform" do
 
       # state_metric_releases = `curl -L -s https://quay.io/api/v1/repository/coreos/kube-state-metrics/tag/?limit=100 | jq '.[] | .[] | .name + " " + .manifest_digest'`
 
-      # Get the sha hash for the kube-state-metrics container
       state_metric_releases = `curl -L -s https://quay.io/api/v1/repository/coreos/kube-state-metrics/tag/?limit=100`
+      # Get the sha hash for the kube-state-metrics container
       sha_list = named_sha_list(state_metric_releases)
-      LOGGING.info "sha_list: #{sha_list}"
+      LOGGING.debug "sha_list: #{sha_list}"
 
       # TODO find hash for image
-      all_pod_sha256 = `kubectl get pods --all-namespaces -o jsonpath='{.items[*].status.containerStatuses[*].imageID}'`
       imageids = KubectlClient::Get.all_container_image_ids
-      LOGGING.info "imageids: #{imageids}"
+      LOGGING.debug "imageids: #{imageids}"
       found = false
       release_name = ""
       sha_list.each do |x|
@@ -50,9 +49,42 @@ namespace "platform" do
         emoji_kube_state_metrics="📶☠️"
         upsert_failed_task("kube_state_metrics", "✖️  FAILURE: Your platform does not have kube state metrics installed #{emoji_kube_state_metrics}")
       end
+    end
+  end
 
-    ensure
-      LOGGING.info "node_failure cleanup"
+  desc "Does the Platform have a Node Exporter installed"
+  task "node_exporter" do |_, args|
+    unless check_poc(args)
+      LOGGING.info "skipping node_exporter: not in poc mode"
+      puts "Skipped".colorize(:yellow)
+      next
+    end
+    LOGGING.info "Running POC: node_exporter"
+    task_response = task_runner(args) do |args|
+      current_dir = FileUtils.pwd 
+
+      # Get the sha hash for the kube-state-metrics container
+      node_exporter = `curl -L -s 'https://registry.hub.docker.com/v2/repositories/prom/node-exporter/tags?page_size=1024'`
+      sha_list = named_sha_list(node_exporter)
+      LOGGING.info "sha_list: #{sha_list}"
+
+      imageids = KubectlClient::Get.all_container_image_ids
+      LOGGING.info "imageids: #{imageids}"
+      found = false
+      release_name = ""
+      sha_list.each do |x|
+        if imageids.find{|i| i.includes?(x["manifest_digest"])}
+          found = true
+          release_name = x["name"]
+        end
+      end
+      if found
+        emoji_node_exporter="📶☠️"
+        upsert_passed_task("node_exporter","✔️  PASSED: Your platform is using the #{release_name} release for the node exporter #{emoji_node_exporter}")
+      else
+        emoji_node_exporter="📶☠️"
+        upsert_failed_task("node_exporter", "✖️  FAILURE: Your platform does not have the node exporter installed #{emoji_node_exporter}")
+      end
     end
   end
 end
@@ -60,7 +92,23 @@ end
 def named_sha_list(resp_json)
   LOGGING.debug "sha_list resp_json: #{resp_json}"
   parsed_json = JSON.parse(resp_json)
-  parsed_json["tags"].not_nil!.as_a.reduce([] of Hash(String, String)) do |acc, i|
-    acc << {"name" => i["name"].not_nil!.as_s, "manifest_digest" => i["manifest_digest"].not_nil!.as_s}
+  LOGGING.debug "sha list parsed json: #{parsed_json}"
+  #TODO if tags then this is a quay repository, otherwise assume docker hub repository
+  if parsed_json["tags"]?
+    parsed_json["tags"].not_nil!.as_a.reduce([] of Hash(String, String)) do |acc, i|
+      acc << {"name" => i["name"].not_nil!.as_s, "manifest_digest" => i["manifest_digest"].not_nil!.as_s}
+    end
+  else
+    parsed_json["results"].not_nil!.as_a.reduce([] of Hash(String, String)) do |acc, i|
+      #TODO always use amd64
+      amd64image = i["images"].as_a.find{|x| x["architecture"].as_s == "amd64"}
+      LOGGING.debug "amd64image: #{amd64image}"
+      if amd64image && amd64image["digest"]?
+        acc << {"name" => i["name"].not_nil!.as_s, "manifest_digest" => amd64image["digest"].not_nil!.as_s}
+      else
+        LOGGING.error "amd64 image not found in #{i["images"]}"
+        acc
+      end
+    end
   end
 end
