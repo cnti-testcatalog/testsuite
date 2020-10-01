122a67803c5cb4735fa373e47a236fd6df60b8d1
denverwilliams <denver@debian.nz>
2020-10-01 18:07:16 -0400

    Add delay before creating workload cluster cncf/cnf-conformance#328

--- spec/platform/cluster_api_spec.cr
+++ spec/platform/cluster_api_spec.cr
@@ -3,23 +3,18 @@ require "colorize"
 
 describe "Platform" do
   before_all do
-    # LOGGING.debug `pwd` 
-    # LOGGING.debug `echo $KUBECONFIG`
-    `./cnf-conformance samples_cleanup`
-    $?.success?.should be_true
     `./cnf-conformance setup`
     $?.success?.should be_true
   end
 
   after_all do
     # cleanup cluster api stuff
-    `kubectl delete -f capd.yaml`
+    `kubectl delete -f cluster-api/capd.yaml`
     `clusterctl delete  --all`
-    `rm -r cluster-api`
+    `rm -rf cluster-api`
   end
 
   it "'clusterapi_enabled' test works" do
-    `./cnf-conformance cleanup`
     `./tools/cluster-api-dev-setup/spec_mock_cluster_api_spec_commands.sh`
     $?.success?.should be_true
     response_s = `./cnf-conformance clusterapi_enabled poc`
--- tools/cluster-api-dev-setup/spec_mock_cluster_api_spec_commands.sh
+++ tools/cluster-api-dev-setup/spec_mock_cluster_api_spec_commands.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Spec test commands
 git clone https://github.com/kubernetes-sigs/cluster-api --depth 1 --branch v0.3.9 
 cd cluster-api
@@ -19,6 +20,8 @@ EOF
 
 clusterctl init  --core cluster-api:v0.3.0  --bootstrap kubeadm:v0.3.0    --control-plane kubeadm:v0.3.0    --infrastructure docker:v0.3.0
 
+sleep 10
+
 CNI_RESOURCES="$(cat test/e2e/data/cni/kindnet/kindnet.yaml)" \
 DOCKER_POD_CIDRS="192.168.0.0/16" \
 DOCKER_SERVICE_CIDRS="172.17.0.0/16" \
@@ -30,9 +33,4 @@ clusterctl config cluster capd --kubernetes-version v1.17.5 \
 --worker-machine-count=2 \
 > capd.yaml
 
-kubectl apply -f capd.yaml
-
-
-# Cleanup done in test
-# kubectl delete -f capd.yaml
-# clusterctl delete  --all
+kubectl apply -f capd.yaml || true
