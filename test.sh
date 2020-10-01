0a431e1619f1f8b8abd84ad5c3f04304e92d1f0c
denverwilliams <denver@debian.nz>
2020-10-01 17:08:10 -0400

    Remove test.sh cncf/cnf-conformance#422

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh -o StrictHostKeyChecking=no -t root@$SSH_HOST GOROOT=/tmp/$TIME/go GOPATH=\$HOME/go PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH kind delete clusters $TIME
-ssh -o StrictHostKeyChecking=no -t root@$SSH_HOST docker rm -f $TIME; ssh -o StrictHostKeyChecking=no -t root@$SSH_HOST rm -rf /tmp/$TIME
-exit 1
