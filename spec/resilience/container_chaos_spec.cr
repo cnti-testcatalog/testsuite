12666c463020e3cabffb2cc5ef21f5fb13ca324e
denverwilliams <denver@debian.nz>
2020-11-02 15:09:57 -0500

    Split resilience tests to speed up specs

--- spec/resilience/container_chaos_spec.cr
+++ spec/resilience/container_chaos_spec.cr
@@ -1,11 +1,11 @@
-require "./spec_helper"
+require "../spec_helper"
 require "colorize"
-require "../src/tasks/utils/utils.cr"
-require "../src/tasks/utils/system_information/helm.cr"
+require "../../src/tasks/utils/utils.cr"
+require "../../src/tasks/utils/system_information/helm.cr"
 require "file_utils"
 require "sam"
 
-describe "Resilience" do
+describe "Resilience Container Chaos" do
   before_all do
     `./cnf-conformance configuration_file_setup`
     $?.success?.should be_true
@@ -35,34 +35,6 @@ describe "Resilience" do
       (/FAILURE: Replicas did not return desired count after container kill test/ =~ response_s).should_not be_nil
     ensure
       `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample-fragile-state deploy_with_chart=false`
-      $?.success?.should be_true
-    end
-  end
-
-  it "'chaos_network_loss' A 'Good' CNF should not crash when network loss occurs", tags: ["chaos_network_loss"]  do
-    begin
-      `./cnf-conformance cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-conformance.yml`
-      $?.success?.should be_true
-      response_s = `./cnf-conformance chaos_network_loss verbose`
-      LOGGING.info response_s
-      $?.success?.should be_true
-      (/PASSED: Replicas available match desired count after network chaos test/ =~ response_s).should_not be_nil
-    ensure
-      `./cnf-conformance cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-conformance.yml`
-      $?.success?.should be_true
-    end
-  end
-
-  it "'chaos_network_loss' A 'Bad' CNF should crash when network loss occurs", tags: ["chaos_network_loss"]  do
-    begin
-      `./cnf-conformance cnf_setup cnf-path=sample-cnfs/sample_network_loss deploy_with_chart=false`
-      $?.success?.should be_true
-      response_s = `./cnf-conformance chaos_network_loss verbose`
-      LOGGING.info response_s
-      $?.success?.should be_true
-      (/FAILURE: Replicas did not return desired count after network chaos test/ =~ response_s).should_not be_nil
-    ensure
-      `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_network_loss deploy_with_chart=false`
       $?.success?.should be_true
     end
   end
---
+++ spec/resilience/network_chaos_spec.cr
@@ -0,0 +1,41 @@
+require "../spec_helper"
+require "colorize"
+require "../../src/tasks/utils/utils.cr"
+require "../../src/tasks/utils/system_information/helm.cr"
+require "file_utils"
+require "sam"
+
+describe "Resilience Network Chaos" do
+  before_all do
+    `./cnf-conformance configuration_file_setup`
+    $?.success?.should be_true
+  end
+
+  it "'chaos_network_loss' A 'Good' CNF should not crash when network loss occurs", tags: ["chaos_network_loss"]  do
+    begin
+      `./cnf-conformance cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-conformance.yml`
+      $?.success?.should be_true
+      response_s = `./cnf-conformance chaos_network_loss verbose`
+      LOGGING.info response_s
+      $?.success?.should be_true
+      (/PASSED: Replicas available match desired count after network chaos test/ =~ response_s).should_not be_nil
+    ensure
+      `./cnf-conformance cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-conformance.yml`
+      $?.success?.should be_true
+    end
+  end
+
+  it "'chaos_network_loss' A 'Bad' CNF should crash when network loss occurs", tags: ["chaos_network_loss"]  do
+    begin
+      `./cnf-conformance cnf_setup cnf-path=sample-cnfs/sample_network_loss deploy_with_chart=false`
+      $?.success?.should be_true
+      response_s = `./cnf-conformance chaos_network_loss verbose`
+      LOGGING.info response_s
+      $?.success?.should be_true
+      (/FAILURE: Replicas did not return desired count after network chaos test/ =~ response_s).should_not be_nil
+    ensure
+      `./cnf-conformance cnf_cleanup cnf-path=sample-cnfs/sample_network_loss deploy_with_chart=false`
+      $?.success?.should be_true
+    end
+  end
+end
