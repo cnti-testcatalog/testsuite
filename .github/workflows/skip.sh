f8099a6d3bae99beb1ced0b9d5acce1d56641bad
denverwilliams <denver@debian.nz>
2020-12-07 19:15:31 -0500

    Don't skip ci & publish if running on a tag

--- .github/workflows/skip.sh
+++ .github/workflows/skip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if git branch | grep -q '^* master$'; then
+if git status | grep -q -P -i 'v[0-9]\.[0-9]'; then
+   echo 'false'
+elif git branch | grep -q '^* master$'; then
    if ! git diff --name-only HEAD HEAD~1 | grep -q -P '^((?!.md).)*$'; then
        echo 'true'
    else
