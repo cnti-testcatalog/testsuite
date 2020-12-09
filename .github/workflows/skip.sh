1e50cd29e3c04e653e99701e9811c2a108dba533
denverwilliams <denver@debian.nz>
2020-12-09 17:43:44 -0500

    check for all versions of master branches e.g. master-jfkdla

--- .github/workflows/skip.sh
+++ .github/workflows/skip.sh
@@ -2,7 +2,7 @@
 
 if git status | grep -q -P -i 'v[0-9]\.[0-9]'; then
    echo 'false'
-elif git branch | grep -q '^* master$'; then
+elif git branch | grep -q '^*.*master.*$'; then
    if ! git diff --name-only HEAD HEAD~1 | grep -q -P '^((?!.md).)*$'; then
        echo 'true'
    else
