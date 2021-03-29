1ba260d281c9ba805761fd32afa9296abe1520a1
sishbi <github@sishbi.me.uk>
2021-03-29 18:26:40 +0100

    Add kubectl.version method Check kubectl version before failing immutable configmap test

--- src/tasks/utils/kubectl_client.cr
+++ src/tasks/utils/kubectl_client.cr
@@ -33,6 +33,26 @@ module KubectlClient
     LOGGING.info "KubectlClient.cp stderr: #{stderr.to_s}"
     {status: status, output: output, error: stderr}
   end
+  def self.version()
+    LOGGING.debug "KubectlClient.version"
+    status = Process.run("kubectl version",
+                         shell: true,
+                         output: output = IO::Memory.new,
+                         error: stderr = IO::Memory.new)
+    LOGGING.debug "KubectlClient.version output: #{output.to_s}"
+    LOGGING.debug "KubectlClient.version stderr: #{stderr.to_s}"
+    # example
+    # Server Version: version.Info{Major:"1", Minor:"18", GitVersion:"v1.18.16", GitCommit:"7a98bb2b7c9112935387825f2fce1b7d40b76236", GitTreeState:"clean", BuildDate:"2021-02-17T11:52:32Z", GoVersion:"go1.13.15", Compiler:"gc", Platform:"linux/amd64"}
+    resp = output.to_s.match /Server Version: version.Info{(Major:"(([0-9]{1,3})"\, )Minor:"([0-9]{1,3}[+]?)")/
+    LOGGING.debug "KubectlClient.version match: #{resp}"
+    if resp
+      version = "#{resp && resp.not_nil![3]}.#{resp && resp.not_nil![4]}"
+    else
+      version = ""
+    end
+    LOGGING.info "KubectlClient.version: #{version}"
+    version
+  end
   module Rollout
     def self.status(deployment_name, timeout="30s")
       #TODO use process command to print both standard out and error
@@ -356,7 +376,7 @@ module KubectlClient
         LOGGING.info("pod_name_prefix: #{pod_name_prefix}")
         if (i[:name] =~ /#{pod_name_prefix}/).nil?
           LOGGING.info "pod_name_prefix: #{pod_name_prefix} does not match #{i[:name]}"
-          acc 
+          acc
         end
         if i[:name] =~ /#{pod_name_prefix}/
           LOGGING.info "pod_name_prefix: #{pod_name_prefix} matches #{i[:name]}"
@@ -389,7 +409,7 @@ module KubectlClient
       # pod = all_pod_names[time_stamps.index(latest_time).not_nil!]
       # pod = all_pods.select{ | x | x =~ /#{pod_name_prefix}/ }
       LOGGING.info "Pods Found: #{pod}"
-      # TODO refactor to return container statuses  
+      # TODO refactor to return container statuses
       status = "#{pod_name_prefix},NotFound,false"
       if pod != "not found"
         status = `kubectl get pods #{pod} -o jsonpath='{.metadata.name},{.status.phase},{.status.containerStatuses[*].ready}'`
