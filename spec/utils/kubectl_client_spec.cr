e5e3097cb6b7c0fc292e699028b6ee50d5e3d43d
denverwilliams <denver@debian.nz>
2021-03-30 14:38:42 -0400

    kubectl nodes now retries

--- spec/utils/kubectl_client_spec.cr
+++ spec/utils/kubectl_client_spec.cr
@@ -1,5 +1,4 @@
 require "../spec_helper"
-require "colorize"
 require "../../src/tasks/utils/utils.cr"
 require "../../src/tasks/dockerd_setup.cr"
 require "../../src/tasks/utils/kubectl_client.cr"
@@ -41,10 +40,20 @@ describe "KubectlClient" do
   end
 
   it "'#KubectlClient.schedulable_nodes' should return all schedulable worker nodes", tags: ["kubectl-nodes"]  do
-    resp = KubectlClient::Get.schedulable_nodes
-    (resp.size).should be > 0
-    (resp[0]).should_not be_nil
-    (resp[0]).should_not be_empty
+    retry_limit = 3
+    retries = 0
+    nodes = nil
+    until (nodes && nodes.size > 0 && !nodes[0].empty?) || retries > retry_limit
+      nodes = KubectlClient::Get.schedulable_nodes
+      retries = retries + 1
+    end
+    # resp = KubectlClient::Get.schedulable_nodes
+    (nodes).should_not be_nil
+    if nodes 
+      (nodes.size).should be > 0
+      (nodes[0]).should_not be_nil
+      (nodes[0]).should_not be_empty
+    end
   end
 
   it "'#KubectlClient.containers' should return all containers defined in a deployment", tags: ["kubectl-pods"]  do
