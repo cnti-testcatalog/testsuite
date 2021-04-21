f01efaea47647ffca00b7240a4434f347571fefe
denverwilliams <denver@debian.nz>
2021-04-21 17:35:00 -0400

    Fix bug with documentation only changes not being skipped

--- .github/workflows/skip.sh
+++ .github/workflows/skip.sh
@@ -2,14 +2,14 @@
 
 if git status | grep -q -P -i 'v[0-9]\.[0-9]'; then
    echo 'false'
-elif git branch | grep -q '^*.*master.*$'; then
+elif git branch | grep -q '^*.*main.*$'; then
    if ! git diff --name-only HEAD HEAD~1 | grep -q -P '^((?!.md).)*$'; then
        echo 'true'
    else
        echo 'false'
    fi
 else
-   if ! git diff --name-only HEAD origin/master | grep -q -P '^((?!.md).)*$'; then
+   if ! git diff --name-only HEAD origin/main | grep -q -P '^((?!.md).)*$'; then
        echo 'true'
    else
        echo 'false'
