6425e7c7d7771e81a7789bc545575d82f9552485
denverwilliams <denver@debian.nz>
2021-06-03 21:12:16 +0000

    Add timeout for litmus check

--- .github/workflows/check_litmus.sh
+++ .github/workflows/check_litmus.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.readyReplicas}') == $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.replicas}') ]] 
+num=0
+until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.readyReplicas}') == $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.replicas}') ]]
 do 
   echo 'waitting until desired Litmus replicas are running'
   sleep 1
+  num=` expr $num + 1`
+  if [[ $num == 20 ]]; then
+    echo "Timeout waitting for Litmus"
+    exit 1
+  fi
 done
