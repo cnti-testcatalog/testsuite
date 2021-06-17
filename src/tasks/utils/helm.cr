0ebd700301d9f38ef816fd1e015fd9428a556b21
wavell <w@vulk.co>
2021-06-17 14:26:30 -0400

    #807 syntax errors

--- src/tasks/utils/helm.cr
+++ src/tasks/utils/helm.cr
@@ -62,7 +62,7 @@ module Helm
     #TODO use Process here
     # Helm template works with either a chart or a directory
     ls_al = `ls -alR #{helm_chart}`
-    LOGGGING.debug "generate_manifest_from_templates ls -alR #{helm_chart}: #{ls_alR}" 
+    LOGGING.debug "generate_manifest_from_templates ls -alR #{helm_chart}: #{ls_al}" 
     template_resp = `#{helm} template #{release_name} #{helm_chart} > #{output_file}`
     input_content = File.read(output_file) 
     LOGGING.debug "generate_manifest_from_templates output_file: #{output_file}"
