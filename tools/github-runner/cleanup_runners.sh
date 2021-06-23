d88910a84abcb0b5a145a225decb4a62061b1dfa
denverwilliams <denver@debian.nz>
2021-06-23 15:54:52 -0400

    Update script to remove all runners cncf/cnf-testsuite#805

--- tools/github-runner/cleanup_runners.sh
+++ tools/github-runner/cleanup_runners.sh
@@ -9,13 +9,15 @@ RUNNERS=(
     139.178.68.167
     86.109.1.243)
 
+TOKEN=$(curl  -XPOST -H "authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/cncf/cnf-testsuite/actions/runners/registration-token | jq -r .token)
 
 for node in "${RUNNERS[@]}"; do
     RUNNER_CONTAINERS=( $(ssh root@$node docker ps --filter "name=github-runner" --format '{{.Names}}' | xargs) )
     ssh root@$node docker exec ${RUNNER_CONTAINERS[1]} "sh -c 'kind delete clusters \$(kind get clusters)'"
-    ssh root@$node docker rm $(docker ps -a -q)
-    ssh root@$node docker rmi -f $(docker images)
     for runner in  "${RUNNER_CONTAINERS[@]}"; do
-        ssh root@$node docker exec $runner "rm -rf /tmp/* || true"
+        ssh root@$node docker exec $runner "/actions-runner/bin/Runner.Listener remove --token $TOKEN"
     done
+    ssh root@$node sudo systemctl stop docker
+    ssh root@$node sudo rm -rf /var/lib/docker/*
+    ssh root@$node sudo systemctl start docker
 done
