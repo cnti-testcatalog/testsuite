10c77a4aa9767b4b42d8e44532a65138329a3bfd
udit <udit@chaosnative.com>
2021-07-01 02:27:52 +0530

    Feat(Resiliency): Add litmus pod delete experiment, timeout for tests and fix test result output
    Signed-off-by: udit <udit@chaosnative.com>

--- src/tasks/litmus_cleanup.cr
+++ src/tasks/litmus_cleanup.cr
@@ -7,12 +7,12 @@ require "./utils/utils.cr"
 desc "Uninstall LitmusChaos"
 task "uninstall_litmus" do |_, args|
     uninstall_chaosengine = `kubectl delete chaosengine --all --all-namespaces`
-    # litmus_uninstall = `kubectl delete -f https://litmuschaos.github.io/litmus/litmus-operator-v1.13.2.yaml`
+    # litmus_uninstall = `kubectl delete -f https://litmuschaos.github.io/litmus/litmus-operator-v1.13.6.yaml`
     if args.named["offline"]?
         LOGGING.info "install litmus offline mode"
-      KubectlClient::Delete.file("#{OFFLINE_MANIFESTS_PATH}/litmus-operator-v1.13.2.yaml")
+      KubectlClient::Delete.file("#{OFFLINE_MANIFESTS_PATH}/litmus-operator-v1.13.6.yaml")
     else
-      KubectlClient::Delete.file("https://litmuschaos.github.io/litmus/litmus-operator-v1.13.2.yaml")
+      KubectlClient::Delete.file("https://litmuschaos.github.io/litmus/litmus-operator-v1.13.6.yaml")
     end
     puts "#{uninstall_chaosengine}" if check_verbose(args)
     # puts "#{litmus_uninstall}" if check_verbose(args)
