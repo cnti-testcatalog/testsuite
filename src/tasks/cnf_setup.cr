5f1c672d18ffb65cb6d6dae9e6d1695d096b654b
Akash Manohar <akash@akash.im>
2021-07-11 17:42:50 +0530

    Use config file instead of extended config file

--- src/tasks/cnf_setup.cr
+++ src/tasks/cnf_setup.cr
@@ -13,7 +13,7 @@ task "cnf_setup", ["helm_local_install"] do |_, args|
   input_file =  cli_hash[:input_file]
   if output_file && !output_file.empty?
     puts "cnf tarball generation mode".colorize(:green)
-    tar_info = AirGap.generate_cnf_setup(cli_hash[:extended_config_file], output_file, cli_hash)
+    tar_info = AirGap.generate_cnf_setup(cli_hash[:config_file], output_file, cli_hash)
     puts "cnf tarball generation mode complete".colorize(:green)
   elsif input_file && !input_file.empty?
     puts "cnf setup airgapped mode".colorize(:green)
