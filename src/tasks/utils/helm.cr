4c4dc5d40702ea5704f8739cc3600f88cbb8fd8f
Akash Manohar <akash@akash.im>
2021-08-02 09:36:36 +0530

    Rename method to be more descriptive

--- src/tasks/utils/helm.cr
+++ src/tasks/utils/helm.cr
@@ -3,24 +3,23 @@ require "colorize"
 require "halite"
 
 module Helm
-
-  #TODO move to kubectlclient
-  DEPLOYMENT="Deployment"
-  SERVICE="Service"
-  POD="Pod"
+  # TODO move to kubectlclient
+  DEPLOYMENT = "Deployment"
+  SERVICE    = "Service"
+  POD        = "Pod"
 
   # Utilities for manifest files that are not templates or have been converted already
   # todo move into own file
   module Manifest
-    def self.parse_manifest_as_ymls(template_file_name="cnfs/temp_template.yml")
+    def self.parse_manifest_as_ymls(template_file_name = "cnfs/temp_template.yml")
       LOGGING.info "parse_manifest_as_ymls template_file_name: #{template_file_name}"
       templates = File.read(template_file_name)
       split_template = templates.split("---")
-      ymls = split_template.map { | template |
-        #TODO strip out NOTES
+      ymls = split_template.map { |template|
+        # TODO strip out NOTES
         YAML.parse(template)
-        # compact seems to have problems with yaml::any
-      }.reject{|x|x==nil}
+      # compact seems to have problems with yaml::any
+      }.reject { |x| x == nil }
       LOGGING.debug "read_template ymls: #{ymls}"
       ymls
     end
@@ -32,12 +31,12 @@ module Helm
       ymls.flatten
     end
 
-    def self.manifest_file_list(manifest_directory, silent=false)
+    def self.manifest_file_list(manifest_directory, silent = false)
       LOGGING.info("manifest_file_list")
       LOGGING.info "manifest_directory: #{manifest_directory}"
       if manifest_directory && !manifest_directory.empty? && manifest_directory != "/"
         LOGGING.info("find: find #{manifest_directory}/ -name *.yml -o -name *.yaml")
-        manifests = `find #{manifest_directory}/ -name "*.yml" -o -name "*.yaml"`.split("\n").select{|x| x.empty? == false}
+        manifests = `find #{manifest_directory}/ -name "*.yml" -o -name "*.yaml"`.split("\n").select { |x| x.empty? == false }
         LOGGING.info("find response: #{manifests}")
         if manifests.size == 0 && !silent
           raise "No manifest ymls found in the #{manifest_directory} directory!"
@@ -47,18 +46,18 @@ module Helm
         [] of String
       end
     end
+
     def self.manifest_containers(manifest_yml)
       LOGGING.debug "manifest_containers: #{manifest_yml}"
       manifest_yml.dig?("spec", "template", "spec", "containers")
     end
   end
 
-
   # Use helm to apply the helm values file to the helm chart templates to create a complete manifest
   # Helm uses manifest files that can be jinja templates
-  def self.generate_manifest_from_templates(release_name, helm_chart, output_file="cnfs/temp_template.yml")
+  def self.generate_manifest_from_templates(release_name, helm_chart, output_file = "cnfs/temp_template.yml")
     LOGGING.debug "generate_manifest_from_templates"
-    # todo remove my guilt 
+    # todo remove my guilt
     helm = CNFSingleton.helm
     LOGGING.info "Helm::generate_manifest_from_templates command: #{helm} template #{release_name} #{helm_chart} > #{output_file}"
     # Helm template works with either a chart or a directory
@@ -66,14 +65,14 @@ module Helm
     LOGGING.info "(before generate) ls -alR #{helm_chart}: #{ls_al}"
     ls_al = `ls -alR cnfs`
     LOGGING.info "(before generate) ls -alR cnfs: #{ls_al}"
-    LOGGING.debug "generate_manifest_from_templates ls -alR #{helm_chart}: #{ls_al}" 
+    LOGGING.debug "generate_manifest_from_templates ls -alR #{helm_chart}: #{ls_al}"
     # template_resp = `#{helm} template #{release_name} #{helm_chart} > #{output_file}`
     resp = Helm.template(release_name, helm_chart, output_file)
     ls_al = `ls -alR #{helm_chart}`
     LOGGING.info "(after generate) ls -alR #{helm_chart}: #{ls_al}"
     ls_al = `ls -alR cnfs`
     LOGGING.info "(after generate) ls -alR cnfs: #{ls_al}"
-    # input_content = File.read(output_file) 
+    # input_content = File.read(output_file)
     LOGGING.debug "generate_manifest_from_templates output_file: #{output_file}"
     [resp[:status].success?, output_file]
   end
@@ -81,26 +80,26 @@ module Helm
   def self.workload_resource_by_kind(ymls : Array(YAML::Any), kind)
     LOGGING.info "workload_resource_by_kind kind: #{kind}"
     LOGGING.debug "workload_resource_by_kind ymls: #{ymls}"
-    resources = ymls.select{|x| x["kind"]?==kind}.reject! {|x|
-        # reject resources that contain the 'helm.sh/hook: test' annotation
+    resources = ymls.select { |x| x["kind"]? == kind }.reject! { |x|
+      # reject resources that contain the 'helm.sh/hook: test' annotation
       LOGGING.debug "x[metadata]?: #{x["metadata"]?}"
       LOGGING.debug "x[metadata][annotations]?: #{x["metadata"]? && x["metadata"]["annotations"]?}"
-      x.dig?("metadata","annotations","helm.sh/hook")
-      }
+      x.dig?("metadata", "annotations", "helm.sh/hook")
+    }
     # end
     LOGGING.debug "resources: #{resources}"
     resources
   end
 
   def self.all_workload_resources(yml : Array(YAML::Any))
-    resources = KubectlClient::WORKLOAD_RESOURCES.map { |k,v|
+    resources = KubectlClient::WORKLOAD_RESOURCES.map { |k, v|
       Helm.workload_resource_by_kind(yml, v)
     }.flatten
     LOGGING.debug "all resource: #{resources}"
     resources
   end
 
-  def self.workload_resource_names(resources : Array(YAML::Any) )
+  def self.workload_resource_names(resources : Array(YAML::Any))
     resource_names = resources.map do |x|
       x["metadata"]["name"]
     end
@@ -108,7 +107,7 @@ module Helm
     resource_names
   end
 
-  def self.workload_resource_kind_names(resources : Array(YAML::Any) )
+  def self.workload_resource_kind_names(resources : Array(YAML::Any))
     resource_names = resources.map do |x|
       {kind: x["kind"], name: x["metadata"]["name"]}
     end
@@ -140,7 +139,7 @@ module Helm
     ret
   end
 
-  def self.helm_gives_k8s_warning?(verbose=false)
+  def self.helm_gives_k8s_warning?(verbose = false)
     helm = CNFSingleton.helm
     stdout = IO::Memory.new
     stderror = IO::Memory.new
@@ -173,13 +172,13 @@ module Helm
     helm_chart_repo.split("/").last
   end
 
-  def self.template(release_name, helm_chart_or_directory, output_file="cnfs/temp_template.yml")
+  def self.template(release_name, helm_chart_or_directory, output_file = "cnfs/temp_template.yml")
     helm = CNFSingleton.helm
     LOGGING.info "helm command: #{helm} template #{release_name} #{helm_chart_or_directory} > #{output_file}"
     status = Process.run("#{helm} template #{release_name} #{helm_chart_or_directory} > #{output_file}",
-                         shell: true,
-                         output: output = IO::Memory.new,
-                         error: stderr = IO::Memory.new)
+      shell: true,
+      output: output = IO::Memory.new,
+      error: stderr = IO::Memory.new)
     LOGGING.info "Helm.template output: #{output.to_s}"
     LOGGING.info "Helm.template stderr: #{stderr.to_s}"
     {status: status, output: output, error: stderr}
@@ -189,13 +188,13 @@ module Helm
     helm = CNFSingleton.helm
     LOGGING.info "helm command: #{helm} install #{cli}"
     status = Process.run("#{helm} install #{cli}",
-                         shell: true,
-                         output: output = IO::Memory.new,
-                         error: stderr = IO::Memory.new)
+      shell: true,
+      output: output = IO::Memory.new,
+      error: stderr = IO::Memory.new)
     Log.info { "Helm.install output: #{output.to_s}" }
     Log.info { "Helm.install stderr: #{stderr.to_s}" }
 
-    if CannotReuseReleaseNameError.content_match?(stderr.to_s)
+    if CannotReuseReleaseNameError.error_text_content_match?(stderr.to_s)
       raise CannotReuseReleaseNameError.new
     end
 
@@ -206,9 +205,9 @@ module Helm
     helm = CNFSingleton.helm
     LOGGING.info "helm command: #{helm} pull #{cli}"
     status = Process.run("#{helm} pull #{cli}",
-                         shell: true,
-                         output: output = IO::Memory.new,
-                         error: stderr = IO::Memory.new)
+      shell: true,
+      output: output = IO::Memory.new,
+      error: stderr = IO::Memory.new)
     LOGGING.info "Helm.install output: #{output.to_s}"
     LOGGING.info "Helm.install stderr: #{stderr.to_s}"
     {status: status, output: output, error: stderr}
@@ -218,16 +217,16 @@ module Helm
     helm = CNFSingleton.helm
     LOGGING.info "helm command: #{helm} fetch #{cli}"
     status = Process.run("#{helm} fetch #{cli}",
-                         shell: true,
-                         output: output = IO::Memory.new,
-                         error: stderr = IO::Memory.new)
+      shell: true,
+      output: output = IO::Memory.new,
+      error: stderr = IO::Memory.new)
     LOGGING.info "Helm.fetch output: #{output.to_s}"
     LOGGING.info "Helm.fetch stderr: #{stderr.to_s}"
     {status: status, output: output, error: stderr}
   end
 
   class CannotReuseReleaseNameError < Exception
-    def self.content_match?(str : String)
+    def self.error_text_content_match?(str : String)
       str.includes? "cannot re-use a name that is still in use"
     end
   end
