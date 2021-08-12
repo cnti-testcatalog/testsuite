846bba4371ecda96e187ed0cfb30b68a2ac24c97
Akash Manohar <akash@akash.im>
2021-08-12 22:25:59 +0530

    Remove unused ls_al var

--- src/tasks/utils/cnf_manager.cr
+++ src/tasks/utils/cnf_manager.cr
@@ -276,8 +276,6 @@ module CNFManager
     Log.info { "config_src: #{config_src}" }
     helm_chart_file = "#{config_src}/#{Helm::CHART_YAML}"
     Log.info { "looking for potential helm_chart_file: #{helm_chart_file}: file exists?: #{File.exists?(helm_chart_file)}" }
-    ls_al = `ls -alR config_src #{config_src}`
-    ls_al = `ls -alR helm_chart_file #{helm_chart_file}`
 
     if !Dir.exists?(config_src) 
       Log.info { "install_method_by_config_src helm_chart selected" }
