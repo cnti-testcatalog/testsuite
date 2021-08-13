7f5d199451a2d39f73fd6cee7d1e4538033fb734
wavell <w@vulk.co>
2021-08-13 15:59:54 -0400

    #920 falco setup and uninstall now present

---
+++ src/tasks/falco_setup.cr
@@ -0,0 +1,39 @@
+require "sam"
+require "file_utils"
+require "colorize"
+require "totem"
+require "./utils/utils.cr"
+
+FALCO_OFFLINE_DIR = "#{TarClient::TAR_REPOSITORY_DIR}/falcosecurity_falco"
+
+desc "Install Falco"
+task "install_falco" do |_, args|
+  helm = BinarySingleton.helm
+  if args.named["offline"]?
+      Log.info { "install falco offline mode" }
+    helm_chart = Dir.entries(FALCO_OFFLINE_DIR).first
+    Helm.install("falco --set ebpf.enabled=true #{FALCO_OFFLINE_DIR}/#{helm_chart}")
+    KubectlClient::Get.resource_wait_for_install("Daemonset", "falco") 
+
+  else
+    Helm.helm_repo_add("falcosecurity","https://falcosecurity.github.io/charts")
+    # needs ebpf parameter for precompiled module 
+    Helm.install("falco --set ebpf.enabled=true falcosecurity/falco")
+    KubectlClient::Get.resource_wait_for_install("Daemonset", "falco") 
+  end
+end
+
+desc "Uninstall Falco"
+task "uninstall_falco" do |_, args|
+  Log.for("verbose").info { "uninstall_falco" } if check_verbose(args)
+  current_dir = FileUtils.pwd
+  helm = BinarySingleton.helm
+  cmd = "#{helm} delete falco > /dev/null 2>&1"
+  status = Process.run(
+    cmd,
+    shell: true,
+    output: output = IO::Memory.new,
+    error: stderr = IO::Memory.new
+  )
+end
+
