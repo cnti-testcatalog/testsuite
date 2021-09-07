499985144c36869ae4431ba071ccb42ff7be6a29
denverwilliams <denver@debian.nz>
2021-09-07 17:55:56 -0400

    Temporarily disable broken chaos-mesh tests cncf/cnf-testsuite#970

--- spec/workload/cpu_hog_spec.cr
+++ spec/workload/cpu_hog_spec.cr
@@ -1,27 +1,27 @@
-require "../spec_helper"
-require "colorize"
-require "../../src/tasks/utils/utils.cr"
-require "../../src/tasks/utils/system_information/helm.cr"
-require "file_utils"
-require "sam"
+# require "../spec_helper"
+# require "colorize"
+# require "../../src/tasks/utils/utils.cr"
+# require "../../src/tasks/utils/system_information/helm.cr"
+# require "file_utils"
+# require "sam"
 
-describe "CPU_ho" do
-  before_all do
-    `./cnf-testsuite configuration_file_setup`
-    $?.success?.should be_true
-  end
+# describe "CPU_hog" do
+#   before_all do
+#     `./cnf-testsuite configuration_file_setup`
+#     $?.success?.should be_true
+#   end
 
-  it "'chaos_cpu_hog' A 'Good' CNF should not crash at 100% cpu usage", tags: ["chaos_cpu_hog"]  do
-    begin
-      `./cnf-testsuite cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
-      $?.success?.should be_true
-      response_s = `./cnf-testsuite chaos_cpu_hog verbose`
-      LOGGING.info response_s
-      $?.success?.should be_true
-      (/PASSED: Application pod is healthy after high CPU consumption/ =~ response_s).should_not be_nil
-    ensure
-      `./cnf-testsuite cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
-      $?.success?.should be_true
-    end
-  end
-end
+#   it "'chaos_cpu_hog' A 'Good' CNF should not crash at 100% cpu usage", tags: ["chaos_cpu_hog"]  do
+#     begin
+#       `./cnf-testsuite cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+#       $?.success?.should be_true
+#       response_s = `./cnf-testsuite chaos_cpu_hog verbose`
+#       LOGGING.info response_s
+#       $?.success?.should be_true
+#       (/PASSED: Application pod is healthy after high CPU consumption/ =~ response_s).should_not be_nil
+#     ensure
+#       `./cnf-testsuite cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+#       $?.success?.should be_true
+#     end
+#   end
+# end
--- spec/workload/resilience/container_chaos_spec.cr
+++ spec/workload/resilience/container_chaos_spec.cr
@@ -1,30 +1,30 @@
-require "../../spec_helper"
-require "colorize"
-require "../../../src/tasks/utils/utils.cr"
-require "../../../src/tasks/utils/system_information/helm.cr"
-require "file_utils"
-require "sam"
+# require "../../spec_helper"
+# require "colorize"
+# require "../../../src/tasks/utils/utils.cr"
+# require "../../../src/tasks/utils/system_information/helm.cr"
+# require "file_utils"
+# require "sam"
 
-describe "Resilience Container Chaos" do
-  before_all do
-    `./cnf-testsuite setup`
-    `./cnf-testsuite configuration_file_setup`
-    $?.success?.should be_true
-  end
+# describe "Resilience Container Chaos" do
+#   before_all do
+#     `./cnf-testsuite setup`
+#     `./cnf-testsuite configuration_file_setup`
+#     $?.success?.should be_true
+#   end
 
-  it "'chaos_container_kill' A 'Good' CNF should recover when its container is killed", tags: ["chaos_container_kill"]  do
-    begin
-      `./cnf-testsuite cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
-      $?.success?.should be_true
-      response_s = `./cnf-testsuite chaos_container_kill verbose`
-      LOGGING.info response_s
-      $?.success?.should be_true
-      (/PASSED: Replicas available match desired count after container kill test/ =~ response_s).should_not be_nil
-    ensure
-      `./cnf-testsuite cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
-      $?.success?.should be_true
-    end
-  end
+#   it "'chaos_container_kill' A 'Good' CNF should recover when its container is killed", tags: ["chaos_container_kill"]  do
+#     begin
+#       `./cnf-testsuite cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+#       $?.success?.should be_true
+#       response_s = `./cnf-testsuite chaos_container_kill verbose`
+#       LOGGING.info response_s
+#       $?.success?.should be_true
+#       (/PASSED: Replicas available match desired count after container kill test/ =~ response_s).should_not be_nil
+#     ensure
+#       `./cnf-testsuite cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+#       $?.success?.should be_true
+#     end
+#   end
 
   # TODO upgrade chaos mesh
   # it "'chaos_container_kill' A 'Bad' CNF should NOT recover when its container is killed", tags: ["chaos_container_kill"]  do
@@ -40,4 +40,4 @@ describe "Resilience Container Chaos" do
   #     $?.success?.should be_true
   #   end
   # end
-end
+# end
