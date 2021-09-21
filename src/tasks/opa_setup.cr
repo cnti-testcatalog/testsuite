b7416c59516be42e64a493cf70e51fd9e08e5f52
denverwilliams <denver@debian.nz>
2021-09-21 15:46:10 -0400

    #955 Add offline mode support for the opa install task

--- src/tasks/opa_setup.cr
+++ src/tasks/opa_setup.cr
@@ -4,25 +4,23 @@ require "colorize"
 require "totem"
 require "./utils/utils.cr"
 
+
+OPA_OFFLINE_DIR = "#{TarClient::TAR_REPOSITORY_DIR}/gatekeeper_gatekeeper"
+
 desc "Sets up OPA in the K8s Cluster"
 task "install_opa" do |_, args|
-  response = String::Builder.new
-  Process.run("echo installing opa", shell: true) do |proc|
+  if args.named["offline"]?
+    LOGGING.info "Intalling OPA Gatekeeper in Offline Mode"
+    chart = Dir.entries(OPA_OFFLINE_DIR).first
+    Helm.install("--set auditInterval=1 --set postInstall.labelNamespace.enabled=false opa-gatekeeper #{OPA_OFFLINE_DIR/#{chart}")
+  else
     Helm.helm_repo_add("gatekeeper","https://open-policy-agent.github.io/gatekeeper/charts")
     Helm.install("--set auditInterval=1 opa-gatekeeper gatekeeper/gatekeeper")
-
-    # `helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts`
-    # `helm install --set auditInterval=1 opa-gatekeeper gatekeeper/gatekeeper`
-
-    while line = proc.output.gets
-      response << line
-      VERBOSE_LOGGING.info "#{line}" if check_verbose(args)
-    end
   end
-  File.write("enforce-image-tag.yml", ENFORCE_IMAGE_TAG)
-  File.write("constraint_template.yml", CONSTRAINT_TEMPLATE)
-  KubectlClient::Apply.file("enforce-image-tag.yml")
-  KubectlClient::Apply.file("constraint_template.yml")
+    File.write("enforce-image-tag.yml", ENFORCE_IMAGE_TAG)
+    File.write("constraint_template.yml", CONSTRAINT_TEMPLATE)
+    KubectlClient::Apply.file("enforce-image-tag.yml")
+    KubectlClient::Apply.file("constraint_template.yml")
 end
 
 desc "Uninstall OPA"
