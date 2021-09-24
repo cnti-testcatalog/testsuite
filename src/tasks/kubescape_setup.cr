afcd7d47bb30b5f8cda97eb088f6f4b99281dcf0
denverwilliams <denver@debian.nz>
2021-09-24 16:29:04 -0400

    Add install task for Kubescape cncf/cnf-testsuite#963

--- src/tasks/kubescape_setup.cr
+++ src/tasks/kubescape_setup.cr
@@ -5,32 +5,33 @@ require "totem"
 require "./utils/utils.cr"
 
 
-OPA_OFFLINE_DIR = "#{TarClient::TAR_REPOSITORY_DIR}/gatekeeper_gatekeeper"
-
 desc "Sets up Kubescape in the K8s Cluster"
 task "install_kubescape" do |_, args|
-  url = "https://raw.githubusercontent.com/armosec/kubescape/v#{KUBESCAPE_VERSION}/install.sh"
+  #TODO Do this with Crystal
+  # version = `curl --silent "https://api.github.com/repos/armosec/kubescape/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+  url = "https://github.com/armosec/kubescape/releases/download/v#{KUBESCAPE_VERSION}/kubescape-ubuntu-latest"
   current_dir = FileUtils.pwd 
   unless Dir.exists?("#{current_dir}/#{TOOLS_DIR}/kubescape")
     FileUtils.mkdir_p("#{current_dir}/#{TOOLS_DIR}/kubescape") 
-    write_file = "#{current_dir}/#{TOOLS_DIR}/kubescape/install.sh"
+    write_file = "#{current_dir}/#{TOOLS_DIR}/kubescape/kubescape"
     Log.info { "url: #{url}" }
     Log.info { "write_file: #{write_file}" }
     resp = Halite.follow.get("#{url}") do |response| 
       File.write("#{write_file}", response.body_io)
     end 
-    status = Process.run("#{current_dir}/#{TOOLS_DIR}/kubescape/install.sh", shell: true, output: stdout, error: stdout)
-    Log.for("verbose").info { stdout }
+    stderr = IO::Memory.new
+    status = Process.run("chmod +x #{write_file}", shell: true, output: stderr, error: stderr)
+    success = status.success?
+    raise "Unable to make #{write_file} executable" if success = false
   end
   #todo kubescape run wrapper
   #kubescape scan framework nsa --exclude-namespaces kube-system,kube-public
   #todo kubescape scrapper
 end
 
-desc "Uninstall OPA"
-task "uninstall_opa" do |_, args|
-  Log.for("verbose").info { "uninstall_opa" } if check_verbose(args)
-  Helm.delete("opa-gatekeeper")
-  KubectlClient::Delete.file("enforce-image-tag.yml")
-  KubectlClient::Delete.file("constraint_template.yml")
+desc "Uninstall Kubescape"
+task "uninstall_kubescape" do |_, args|
+  current_dir = FileUtils.pwd 
+  Log.for("verbose").info { "uninstall_kubescape" } if check_verbose(args)
+  FileUtils.rm_rf("#{current_dir}/#{TOOLS_DIR}/kubescape")
 end
