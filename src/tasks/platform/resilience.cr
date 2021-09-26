9a5c0e2d5d82f7ab158a32659b37f1fd1c2dfb38
Akash Manohar <akash@akash.im>
2021-09-26 09:45:08 +0530

    Replace backticks in resilience.cr with Process.run

--- src/tasks/platform/resilience.cr
+++ src/tasks/platform/resilience.cr
@@ -25,17 +25,15 @@ namespace "platform" do
       helm = BinarySingleton.helm
 
       #Select the first node that isn't a master and is also schedulable
-      worker_nodes = `kubectl get nodes --selector='!node-role.kubernetes.io/master' -o 'go-template={{range .items}}{{$taints:=""}}{{range .spec.taints}}{{if eq .effect "NoSchedule"}}{{$taints = print $taints .key ","}}{{end}}{{end}}{{if not $taints}}{{.metadata.name}}{{ "\\n"}}{{end}}{{end}}'`
-      worker_node = worker_nodes.split("\n")[0]
-
+      worker_nodes = KubectlClient::Get.worker_nodes
+      worker_node = worker_nodes[0]
 
       File.write("node_failure_values.yml", NODE_FAILED_VALUES)
-      install_coredns = `#{helm} install node-failure -f ./node_failure_values.yml --set nodeSelector."kubernetes\\.io/hostname"=#{worker_node} stable/coredns`
+      install_coredns = Helm.install("node-failure -f ./node_failure_values.yml --set nodeSelector.\"kubernetes\\.io/hostname\"=#{worker_node} stable/coredns")
       KubectlClient::Get.wait_for_install("node-failure-coredns")
 
-
       File.write("reboot_daemon_pod.yml", REBOOT_DAEMON)
-      install_reboot_daemon = `kubectl create -f reboot_daemon_pod.yml`
+      KubectlClient::Apply.file("reboot_daemon_pod.yml")
       KubectlClient::Get.wait_for_install("node-failure-coredns")
 
       pod_ready = ""
@@ -55,7 +53,7 @@ namespace "platform" do
 
         # Find Reboot Daemon name
         reboot_daemon_pod = KubectlClient::Get.pod_status("reboot", "--field-selector spec.nodeName=#{worker_node}").split(",")[0]
-        start_reboot = `kubectl exec -ti #{reboot_daemon_pod} touch /tmp/reboot`
+        start_reboot = KubectlClient.exec("-ti #{reboot_daemon_pod} touch /tmp/reboot")
 
         #Watch for Node Failure.
         pod_ready = ""
@@ -100,7 +98,7 @@ namespace "platform" do
       ensure
         Log.info { "node_failure cleanup" }
         delete_reboot_daemon = KubectlClient::Delete.file("reboot_daemon_pod.yml")
-        delete_coredns = `#{helm} delete node-failure`
+        delete_coredns = Helm.delete("node-failure")
         File.delete("reboot_daemon_pod.yml")
         File.delete("node_failure_values.yml")
       end
