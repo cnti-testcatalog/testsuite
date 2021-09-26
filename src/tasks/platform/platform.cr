1ff501cb246844b85d44a104de64fedae1bc5d3d
Akash Manohar <akash@akash.im>
2021-09-26 11:20:57 +0530

    Updates to platform.cr for bug/307

--- src/tasks/platform/platform.cr
+++ src/tasks/platform/platform.cr
@@ -27,23 +27,44 @@ task "k8s_conformance" do |_, args|
     sonobuoy = "#{current_dir}/#{TOOLS_DIR}/sonobuoy/sonobuoy"
 
     # Clean up old results
-    delete = `#{sonobuoy} delete --all --wait`
-    VERBOSE_LOGGING.info delete if check_verbose(args)
+    delete_cmd = "#{sonobuoy} delete --all --wait"
+    Process.run(
+      delete_cmd,
+      shell: true,
+      output: delete_stdout = IO::Memory.new,
+      error: delete_stderr = IO::Memory.new
+    )
+    Log.for("verbose").info { delete_stdout } if check_verbose(args)
 
     # Run the tests
-    testrun = ""
-    VERBOSE_LOGGING.info ENV["CRYSTAL_ENV"]? if check_verbose(args)
+    testrun_stdout = IO::Memory.new
+    Log.for("verbose").info { "CRYSTAL_ENV: #{ENV["CRYSTAL_ENV"]?}" } if check_verbose(args)
     if ENV["CRYSTAL_ENV"]? == "TEST"
-      LOGGING.info("Running Sonobuoy using Quick Mode")
-      testrun = `#{sonobuoy} run --wait --mode quick`
+      Log.info { "Running Sonobuoy using Quick Mode" }
+      cmd = "#{sonobuoy} run --wait --mode quick"
+      Process.run(
+        cmd,
+        shell: true,
+        output: testrun_stdout,
+        error: testrun_stderr = IO::Memory.new
+      )
     else
-      LOGGING.info("Running Sonobuoy Conformance")
-      testrun = `#{sonobuoy} run --wait`
+      Log.info { "Running Sonobuoy Conformance" }
+      cmd = "#{sonobuoy} run --wait"
+      Process.run(
+        cmd,
+        shell: true,
+        output: testrun_stdout,
+        error: testrun_stderr = IO::Memory.new
+      )
     end
-    VERBOSE_LOGGING.info testrun if check_verbose(args)
+    Log.for("verbose").info { testrun_stdout.to_s } if check_verbose(args)
 
-    results = `results=$(#{sonobuoy} retrieve); #{sonobuoy} results $results`
-    VERBOSE_LOGGING.info results if check_verbose(args)
+    cmd = "results=$(#{sonobuoy} retrieve); #{sonobuoy} results $results"
+    results_stdout = IO::Memory.new
+    Process.run(cmd, shell: true, output: results_stdout, error: results_stdout)
+    results = results_stdout.to_s
+    Log.for("verbose").info { results } if check_verbose(args)
 
     # Grab the failed line from the results
 
@@ -55,13 +76,12 @@ task "k8s_conformance" do |_, args|
       upsert_passed_task("k8s_conformance", "✔️  PASSED: K8s conformance test has no failures")
     end
   rescue ex
-    LOGGING.error ex.message
+    Log.error { ex.message }
     ex.backtrace.each do |x|
-      LOGGING.error x
+      Log.error { x }
     end
   ensure
-    remove_tar = `rm *sonobuoy*.tar.gz`
-    VERBOSE_LOGGING.debug remove_tar if check_verbose(args)
+    FileUtils.rm_rf(Dir.glob("*sonobuoy*.tar.gz"))
   end
 end
 
@@ -69,13 +89,13 @@ desc "Is Cluster Api available and managing a cluster?"
 task "clusterapi_enabled" do |_, args|
   CNFManager::Task.task_runner(args) do
     unless check_poc(args)
-      LOGGING.info "skipping clusterapi_enabled: not in poc mode"
+      Log.info { "skipping clusterapi_enabled: not in poc mode" }
       puts "SKIPPED: ClusterAPI Enabled".colorize(:yellow)
       next
     end
 
-    VERBOSE_LOGGING.info "clusterapi_enabled" if check_verbose(args)
-    LOGGING.info("clusterapi_enabled args #{args.inspect}")
+    Log.for("verbose").info { "clusterapi_enabled" } if check_verbose(args)
+    Log.info { "clusterapi_enabled args #{args.inspect}" }
 
     # We test that the namespaces for cluster resources exist by looking for labels
     # I found those by running
@@ -84,14 +104,20 @@ task "clusterapi_enabled" do |_, args|
     # https://cluster-api.sigs.k8s.io/clusterctl/commands/init.html#additional-information
 
     # this indicates that cluster-api is installed
-    clusterapi_namespaces_output = `kubectl get namespaces --selector clusterctl.cluster.x-k8s.io -o json`
-    clusterapi_namespaces_json = JSON.parse(clusterapi_namespaces_output)
-
-    LOGGING.info("clusterapi_namespaces_json: #{clusterapi_namespaces_json}")
+    clusterapi_namespaces_json = KubectlClient::Get.namespaces(
+      "--selector clusterctl.cluster.x-k8s.io"
+    )
+    Log.info { "clusterapi_namespaces_json: #{clusterapi_namespaces_json}" }
 
     # check that a node is actually being manageed
     # TODO: suppress msg in the case that this resource does-not-exist which is what happens when cluster-api is not installed
-    clusterapi_control_planes_output = `kubectl get kubeadmcontrolplanes.controlplane.cluster.x-k8s.io -o json`
+    cmd = "kubectl get kubeadmcontrolplanes.controlplane.cluster.x-k8s.io -o json"
+    Process.run(
+      cmd,
+      shell: true,
+      output: clusterapi_control_planes_output = IO::Memory.new,
+      error: stderr = IO::Memory.new
+    )
 
     proc_clusterapi_control_planes_json = -> do
       begin
@@ -103,7 +129,7 @@ task "clusterapi_enabled" do |_, args|
     end
 
     clusterapi_control_planes_json = proc_clusterapi_control_planes_json.call
-    LOGGING.info("clusterapi_control_planes_json: #{clusterapi_control_planes_json}")
+    Log.info { "clusterapi_control_planes_json: #{clusterapi_control_planes_json}" }
 
     emoji_control="✨"
     if clusterapi_namespaces_json["items"]? && clusterapi_namespaces_json["items"].as_a.size > 0 && clusterapi_control_planes_json["items"]? && clusterapi_control_planes_json["items"].as_a.size > 0
