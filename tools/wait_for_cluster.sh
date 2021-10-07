bc0fb7b2ae82d511d5113107ecb87e81dad30bfa
denverwilliams <denver@debian.nz>
2021-10-07 16:20:12 -0400

    Fix wait for cluster cncf/cnf-testsuite#989

--- tools/wait_for_cluster.sh
+++ tools/wait_for_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running Wait for Cluster"
-until [[ $(kubectl get pods --namespace=kube-system --field-selector=status.phase=Running | wc -l) == $(kubectl get pods --namespace=kube-system | wc -l) ]]
+until [[ $(kubectl get pods --all-namespaces) && $(kubectl get pods --namespace=kube-system --field-selector=status.phase=Running | wc -l) == $(kubectl get pods --namespace=kube-system | wc -l) ]]
 do
   echo "Waiting for system pods to be ready"
   echo "System Pod Count: $(kubectl get pods --namespace=kube-system | wc -l)" 
