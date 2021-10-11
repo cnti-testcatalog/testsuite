32576e55fb634d68840c3f25259f74b7a90621e9
denverwilliams <denver@debian.nz>
2021-10-11 19:44:32 +0000

    Update the Litmus check to work with cached clusters cncf/cnf-testsuite#989

--- .github/workflows/check_litmus.sh
+++ .github/workflows/check_litmus.sh
@@ -2,7 +2,7 @@
 
 num=0
 
-until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce) ]]
+until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.replicas}') -ge 1 ]]
 do 
     echo 'Waiting until Litmus Deployment exists'
     sleep 1
@@ -16,7 +16,7 @@ done
 
 until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.readyReplicas}') == $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.replicas}') ]]
 do 
-  echo 'waiting until desired Litmus replicas are running'
+  echo 'Waiting until desired Litmus Replicas are running'
   sleep 1
   num=` expr $num + 1`
   if [[ $num == 20 ]]; then
