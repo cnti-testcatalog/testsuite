07c3eec9304d8e851344d5b780db2fe194a5cb24
denverwilliams <denver@debian.nz>
2021-10-11 13:22:07 -0400

    Update Litmus check to wait until the deployment exists cncf/cnf-testsuite#989

--- .github/workflows/check_litmus.sh
+++ .github/workflows/check_litmus.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 num=0
+
+until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce) ]]
+do 
+    echo 'Waiting until Litmus Deployment exists'
+    sleep 1
+    num=` expr $num + 1`
+    if [[ $num == 20 ]]; then
+        echo "Timeout waiting for Litmus"
+        exit 1
+    fi
+done
+
+
 until [[ $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.readyReplicas}') == $(kubectl get deployment --namespace=litmus chaos-operator-ce -o=jsonpath='{.status.replicas}') ]]
 do 
   echo 'waiting until desired Litmus replicas are running'
