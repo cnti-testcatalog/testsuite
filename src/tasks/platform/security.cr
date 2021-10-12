b40b445f43fed46720102a45ea67fbb9ef08a260
wavell <w@vulk.co>
2021-10-12 14:23:37 -0400

    #996 cluster_admin is now a platform test

--- src/tasks/platform/security.cr
+++ src/tasks/platform/security.cr
@@ -5,7 +5,7 @@ require "../utils/utils.cr"
 
 namespace "platform" do
   desc "The CNF test suite checks to see if the platform is hardened."
-  task "security", ["control_plane_hardening"] do |t, args|
+  task "security", ["control_plane_hardening", "cluster_admin"] do |t, args|
     Log.for("verbose").info { "security" } if check_verbose(args)
     stdout_score("platform:security")
   end
@@ -26,6 +26,27 @@ namespace "platform" do
       Kubescape.alerts_by_test(test_json).map{|t| puts "\n#{t}".colorize(:red)}
       puts "Remediation: #{Kubescape.remediation(test_json)}\n".colorize(:red)
       resp
+    end
+    end
+  end
+
+  desc "Attackers who have Cluster-admin permissions (can perform any action on any resource), can take advantage of their high privileges for malicious intentions. Determines which subjects have cluster admin permissions."
+  task "cluster_admin", ["kubescape_scan"] do |_, args|
+    unless args.named["offline"]?
+        CNFManager::Task.task_runner(args) do |args, config|
+      VERBOSE_LOGGING.info "cluster_admin" if check_verbose(args)
+      results_json = Kubescape.parse
+      test_json = Kubescape.test_by_test_name(results_json, "Cluster-admin binding")
+
+      emoji_security="🔓🔑"
+      if Kubescape.test_passed?(test_json) 
+        upsert_passed_task("cluster_admin", "✔️  PASSED: No users with cluster admin role found #{emoji_security}")
+      else
+        resp = upsert_failed_task("cluster_admin", "✖️  FAILED: Users with cluster admin role found #{emoji_security}")
+        Kubescape.alerts_by_test(test_json).map{|t| puts "\n#{t}".colorize(:red)}
+        puts "Remediation: #{Kubescape.remediation(test_json)}\n".colorize(:red)
+        resp
+      end
     end
     end
   end
