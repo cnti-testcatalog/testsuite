2b5cc8643a14c0cad432fe9dc54ae5ceb64dbefa
Akash Manohar <akash@akash.im>
2021-10-18 23:05:25 +0530

    Add github workflow step for running opa tests in airgapped mode

--- src/tasks/utils/opa.cr
+++ src/tasks/utils/opa.cr
@@ -8,11 +8,11 @@ module OPA
   OPA_KIND_NAME="requiretags"
   OPA_VIOLATION_NAME="block-latest-tag"
   def self.find_non_versioned_pod(pod_name)
-    LOGGING.info "OPA.find_non_versioned_pod: #{pod_name}"
+    Log.info { "OPA.find_non_versioned_pod: #{pod_name}" }
     resp = KubectlClient.describe(OPA_KIND_NAME, OPA_VIOLATION_NAME)
     output = resp[:output]
     match = output.to_s.match(/.*Pod #{pod_name}, it uses an image tag that is not versioned.*/)
-    LOGGING.info "OPA Pod Data: #{match}"
+    Log.info { "OPA Pod Data: #{match}" }
     if match
       true
     else
