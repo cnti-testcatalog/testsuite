03b3be52d5297b5cd7f5e52b685d8901711a01da
Akash Manohar <akash@akash.im>
2021-10-19 00:03:45 +0530

    Add test for kubescape exposed dashboard

--- src/tasks/workload/security.cr
+++ src/tasks/workload/security.cr
@@ -242,3 +242,24 @@ task "dangerous_capabilities", ["kubescape_scan"] do |_, args|
     end
   end
 end
+
+desc "Check if the cluster has an exposed dashboard"
+task "exposed_dashboard", ["kubescape_scan"] do |_, args|
+  next if args.named["offline"]?
+
+  CNFManager::Task.task_runner(args) do |args, config|
+    Log.for("verbose").info { "exposed_dashboard" } if check_verbose(args)
+    results_json = Kubescape.parse
+    test_json = Kubescape.test_by_test_name(results_json, "Exposed dashboard")
+
+    emoji_security = "🔓🔑"
+    if Kubescape.test_passed?(test_json)
+      upsert_passed_task("exposed_dashboard", "✔️  PASSED: No exposed dashboard found in the cluster #{emoji_security}")
+    else
+      resp = upsert_failed_task("exposed_dashboard", "✖️  FAILED: Found exposed dashboard in the cluster #{emoji_security}")
+      Kubescape.alerts_by_test(test_json).map{|t| puts "\n#{t}".colorize(:red)}
+      puts "Remediation: #{Kubescape.remediation(test_json)}\n".colorize(:red)
+      resp
+    end
+  end
+end
