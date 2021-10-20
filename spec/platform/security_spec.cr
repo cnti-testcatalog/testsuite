81e86aa189cd5ffd370336cf8baa43148e2eead5
Akash Manohar <akash@akash.im>
2021-10-20 22:14:56 +0530

    Add exposed_dashboard to points.yml

--- spec/platform/security_spec.cr
+++ spec/platform/security_spec.cr
@@ -7,6 +7,7 @@ describe "Platform" do
     `./cnf-testsuite setup`
     $?.success?.should be_true
   end
+
   it "'control_plane_hardening' should pass if the control plane has been hardened", tags: ["platform:security"] do
     response_s = `./cnf-testsuite platform:control_plane_hardening`
     LOGGING.info response_s
