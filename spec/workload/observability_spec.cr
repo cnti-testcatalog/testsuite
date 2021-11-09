64a7aae105a790fde42f59daca6ca4fd26ca9451
Akash Manohar <akash@akash.im>
2021-11-09 12:13:01 +0530

    Add good-cnf test for log_output

---
+++ spec/workload/observability_spec.cr
@@ -0,0 +1,17 @@
+require "../spec_helper"
+require "colorize"
+require "../../src/tasks/utils/utils.cr"
+
+describe "Observability" do
+  it "'log_output' should pass with a cnf that outputs logs to stdout", tags: ["observability"]  do
+    begin
+      LOGGING.info `./cnf-testsuite cnf_setup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+      response_s = `./cnf-testsuite log_output verbose`
+      LOGGING.info response_s
+      $?.success?.should be_true
+      (/PASSED: Resources output logs to stdout and stderr/ =~ response_s).should_not be_nil
+    ensure
+      LOGGING.info `./cnf-testsuite cnf_cleanup cnf-config=sample-cnfs/sample-coredns-cnf/cnf-testsuite.yml`
+    end
+  end
+end
