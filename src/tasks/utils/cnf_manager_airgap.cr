421a443d478feea8ba42ddb735e58e1a64b6aac5
denverwilliams <denver@debian.nz>
2021-11-11 01:13:54 -0500

    Manually Tar Calico Helm Chart cncf/cnf-testsuite#1064

--- src/tasks/utils/cnf_manager_airgap.cr
+++ src/tasks/utils/cnf_manager_airgap.cr
@@ -161,7 +161,7 @@ module CNFManager
       FileUtils.rm_rf("#{TarClient::TAR_TMP_BASE}/#{download_path}")
       FileUtils.mkdir_p("#{TarClient::TAR_TMP_BASE}/" + download_path)
       `./tools/kubescape/kubescape download framework nsa --output #{TarClient::TAR_DOWNLOAD_DIR}/nsa.json`
-      TarClient.append(output_file, TarClient::TAR_TMP_BASE, "#{download_path}nsa.json")
+      TarClient.append(output_file, TarClient::TAR_TMP_BASE, "#{download_path}/nsa.json")
       Helm.helm_repo_add("chaos-mesh", "https://charts.chaos-mesh.org")
       # todo create helm chart configuration yaml that includes all chart elements for specs
       AirGap.tar_helm_repo("chaos-mesh/chaos-mesh --version 0.5.1", output_file)
@@ -170,6 +170,13 @@ module CNFManager
       # Calico Helm  Relase Can't be Pinned, Download Directly
       # Helm.helm_repo_add("projectcalico","https://docs.projectcalico.org/charts")
       # AirGap.tar_helm_repo("projectcalico/tigera-operator --version v3.20.2", output_file)
+
+      tar_dir = AirGap.helm_tar_dir("projectcalico/tigera-operator")
+      info = AirGap.tar_info_by_config_src("projectcalico/tigera-operator")
+      tar_name = info[:tar_name]
+      Helm.fetch("https://github.com/projectcalico/calico/releases/download/v3.20.2/tigera-operator-v3.20.2-1.tgz -d #{tar_dir}")
+      Log.info { "TarDir: #{tar_dir}, TarName: #{tar_name}" }
+      TarClient.append(output_file, TarClient::TAR_TMP_BASE, "#{tar_dir}/#{tar_name}")
 
       Helm.helm_repo_add("cilium","https://helm.cilium.io/")
       AirGap.tar_helm_repo("cilium/cilium --version 1.10.5", output_file)
