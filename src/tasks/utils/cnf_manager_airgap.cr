22ada01168d239b86d882ac2dc44554484744b72
denverwilliams <denver@debian.nz>
2021-11-11 00:46:07 -0500

    Disable Calico Airgap code cncf/cnf-testsuite#1064

--- src/tasks/utils/cnf_manager_airgap.cr
+++ src/tasks/utils/cnf_manager_airgap.cr
@@ -170,19 +170,6 @@ module CNFManager
       # Calico Helm  Relase Can't be Pinned, Download Directly
       # Helm.helm_repo_add("projectcalico","https://docs.projectcalico.org/charts")
       # AirGap.tar_helm_repo("projectcalico/tigera-operator --version v3.20.2", output_file)
-      tar_dir = AirGap.helm_tar_dir("projectcalico/tigera-operator")
-      info = AirGap.tar_info_by_config_src("projectcalico/tigera-operator")
-      repo_dir = info[:repo_dir]
-      tar_name = info[:tar_name]
-      repo_path = info[:repo_path]
-      FileUtils.mkdir_p(tar_dir)
-      Helm.fetch("https://github.com/projectcalico/calico/releases/download/v3.20.2/tigera-operator-v3.20.2-1.tgz -d #{tar_dir}")
-      TarClient.modify_tar!(tar_name) do |directory| 
-        template_files = Find.find(directory, "*.yaml*", "100")
-        template_files.map{|x| AirGap.image_pull_policy(x)}
-      end
-      TarClient.append(output_file, "/tmp", "#{repo_path}")
-      FileUtils.rm_rf("/tmp/#{repo_path}")
 
       Helm.helm_repo_add("cilium","https://helm.cilium.io/")
       AirGap.tar_helm_repo("cilium/cilium --version 1.10.5", output_file)
