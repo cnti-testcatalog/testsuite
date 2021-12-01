42aefcdd238d3880cb4d6bcc9707389365470254
Akash Manohar <akash@akash.im>
2021-12-01 08:44:59 +0530

    Rescue helm install falco if falco is already installed

--- src/tasks/falco_setup.cr
+++ src/tasks/falco_setup.cr
@@ -15,14 +15,18 @@ task "install_falco" do |_, args|
     image_tag = "--set image.tag=0.29.1"
     chart_version = "--version 1.15.7"
   end
-  if args.named["offline"]?
-      Log.info { "install falco offline mode" }
-    helm_chart = Dir.entries(FALCO_OFFLINE_DIR).first
-    Helm.install("falco --set ebpf.enabled=true #{chart_version} #{image_arg} #{image_tag} -f ./falco_rule.yaml #{FALCO_OFFLINE_DIR}/#{helm_chart}")
-  else
-    Helm.helm_repo_add("falcosecurity","https://falcosecurity.github.io/charts")
-    # needs ebpf parameter for precompiled module 
-    Helm.install("falco --set ebpf.enabled=true #{chart_version} #{image_arg} #{image_tag} -f ./falco_rule.yaml falcosecurity/falco")
+  begin
+    if args.named["offline"]?
+        Log.info { "install falco offline mode" }
+      helm_chart = Dir.entries(FALCO_OFFLINE_DIR).first
+      Helm.install("falco --set ebpf.enabled=true #{chart_version} #{image_arg} #{image_tag} -f ./falco_rule.yaml #{FALCO_OFFLINE_DIR}/#{helm_chart}")
+    else
+      Helm.helm_repo_add("falcosecurity","https://falcosecurity.github.io/charts")
+      # needs ebpf parameter for precompiled module
+      Helm.install("falco --set ebpf.enabled=true #{chart_version} #{image_arg} #{image_tag} -f ./falco_rule.yaml falcosecurity/falco")
+    end
+  rescue Helm::CannotReuseReleaseNameError
+    Log.info { "Falco already installed" }
   end
 end
 
