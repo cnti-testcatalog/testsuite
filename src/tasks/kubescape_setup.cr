0b39dea0827bd11b9ce3c71fb7b08bea94ba87fc
agentpoyo <drew.bentley@gmail.com>
2021-12-03 11:19:01 -0500

    #1121 Add debug line for kubescape framework file

--- src/tasks/kubescape_setup.cr
+++ src/tasks/kubescape_setup.cr
@@ -52,7 +52,7 @@ task "install_kubescape" do |_, args|
             File.write(framework_path, response.body_io)
           end
         end
-
+        `head #{framework_path}`
       end
     end
   end
