1e24d288d599219069668823a97e8a4fba8e2bdc
denverwilliams <denver@debian.nz>
2021-12-03 21:34:24 -0500

    Add script for setting up the kvm runners

---
+++ tools/github-runner/setup_kvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RUNNERS=(
+    139.178.91.101)
+
+for node in "${!RUNNERS[@]}"; do
+    ssh root@${RUNNERS[$node]} "sudo apt update && sudo apt -y install wget curl unzip qemu-kvm libvirt-daemon  bridge-utils virtinst libvirt-daemon-system virt-top libguestfs-tools libosinfo-bin  qemu-system virt-manager"
+    TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+    ssh root@${RUNNERS[$node]} "wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip && unzip terraform_${TER_VER}_linux_amd64.zip && sudo mv terraform /usr/local/bin/"
+    ssh root@${RUNNERS[$node]} "sudo modprobe vhost_net && sudo systemctl start libvirtd && sudo systemctl enable libvirtd && echo vhost_net | sudo tee -a /etc/modules && echo 'security_driver = \"none\"' | sudo tee -a /etc/libvirt/qemu.conf && systemctl restart libvirtd"
+    # ssh root@${RUNNERS[$node]} "sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs"
+    ssh root@${RUNNERS[$node]} "sudo virsh pool-define-as default dir - - - - '/guest_images' && sudo virsh pool-build default && sudo virsh pool-start default"
+done
