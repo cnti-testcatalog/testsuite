a4db8cc0d5f8426d98e1870960c17d04c8b823b5
denverwilliams <denver@debian.nz>
2021-12-06 21:45:56 +0000

    Fix bugs with create runner scripts

--- tools/github-runner/kvm/create_runners.sh
+++ tools/github-runner/kvm/create_runners.sh
@@ -7,8 +7,8 @@ VIPS=(
 147.75.68.32/28)
 
 for node in "${!RUNNERS[@]}"; do
-    ssh root@${RUNNERS[$node]} "git clone https://github.com/cncf/cnf-testsuite.git /"
-    ssh root@${RUNNERS[$node]} "cd /cnf-testsuite/ && git checkout github_vms && cd tools/github-runner/kvm/terraform && terraform init"
-    ssh root@${RUNNERS[$node]} \"cd /cnf-testsuite/tools/github-runner/kvm/terraform && terraform apply -var="token=${TOKEN}" -var="elastic_ips=${VIPS[$node]}"\"
+    ssh root@${RUNNERS[$node]} "git clone https://github.com/cncf/cnf-testsuite.git /home/cnf-testsuite"
+    ssh root@${RUNNERS[$node]} "cd /home/cnf-testsuite/ && git checkout github_vms && cd tools/github-runner/kvm/terraform && terraform init"
+    ssh root@${RUNNERS[$node]} "cd /home/cnf-testsuite/tools/github-runner/kvm/terraform && terraform apply -auto-approve -var=\"token=${TOKEN}\" -var=\"elastic_ips=${VIPS[$node]}\""
 done
 
