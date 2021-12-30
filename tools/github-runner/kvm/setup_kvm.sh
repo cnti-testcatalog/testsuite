c2a556bdd39221a74ed1aa55de0d84afe047050a
denverwilliams <denver@debian.nz>
2021-12-30 17:28:14 -0500

    Add support for lvm

--- tools/github-runner/kvm/setup_kvm.sh
+++ tools/github-runner/kvm/setup_kvm.sh
@@ -13,13 +13,19 @@ for node in "${!RUNNERS[@]}"; do
     ssh root@${RUNNERS[$node]} "wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip && unzip terraform_${TER_VER}_linux_amd64.zip && sudo mv terraform /usr/local/bin/"
     ssh root@${RUNNERS[$node]} "sudo modprobe vhost_net && sudo systemctl start libvirtd && sudo systemctl enable libvirtd && echo vhost_net | sudo tee -a /etc/modules && echo 'security_driver = \"none\"' | sudo tee -a /etc/libvirt/qemu.conf && systemctl restart libvirtd"
     ssh root@${RUNNERS[$node]} "sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs"
-    ssh root@${RUNNERS[$node]} "sudo virsh pool-define-as default dir - - - - '/guest_images' && sudo virsh pool-build default && sudo virsh pool-start default"
+#    ssh root@${RUNNERS[$node]} "sudo virsh pool-define-as default dir - - - - '/guest_images' && sudo virsh pool-build default && sudo virsh pool-start default"
     ssh root@${RUNNERS[$node]} 'sudo sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf'
     ssh root@${RUNNERS[$node]} 'sudo sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf'
     ssh root@${RUNNERS[$node]} 'sudo sysctl -p'
-    ssh root@${RUNNERS[$node]} 'sudo apt install -y libvirt-daemon-driver-storage-zfs'
-    ssh root@${RUNNERS[$node]} 'sudo zpool create -f zfspool /dev/sdb'
-    ssh root@${RUNNERS[$node]} 'virsh pool-define-as --name zfsfilepool --source-name zfspool --type zfs'
-    ssh root@${RUNNERS[$node]} 'virsh pool-start zfsfilepool'
-    scp ./qemu-images/my-build.qcow2 root@${RUNNERS[$node]}:/my-build.qcow2
+#    ssh root@${RUNNERS[$node]} 'sudo apt install -y libvirt-daemon-driver-storage-zfs'
+#    ssh root@${RUNNERS[$node]} 'sudo zpool create -f zfspool /dev/sdb'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-define-as --name zfsfilepool --source-name zfspool --type zfs'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-start zfsfilepool'
+    scp ./qemu-images/my-build.qcow2 root@${RUNNERS[$node]}:/my-build.raw
+    ssh root@${RUNNERS[$node]} 'qemu-img resize -f raw /my-build.raw +40G'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-define-as default logical - - /dev/sdb libvirt_lvm \ /dev/libvirt_lvm'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-build default'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-start default'
+#    ssh root@${RUNNERS[$node]} 'virsh pool-autostart default'
+
 done
