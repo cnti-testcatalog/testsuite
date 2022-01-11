0070c985a00f92e17c237fcaca3a53c9cba71cca
denverwilliams <denver@debian.nz>
2022-01-11 23:05:06 +0000

    Use input_file to determine offline mode cncf/cnf-testsuite#1030

--- src/tasks/cluster_setup.cr
+++ src/tasks/cluster_setup.cr
@@ -10,23 +10,12 @@ require "./utils/utils.cr"
 desc "Install CNF Test Suite Cluster Tools"
 task "install_cluster_tools" do |_, args|
   Log.info { "install_cluster_tools" }
+  cli_hash = CNFManager.sample_setup_cli_args(args)
+  input_file =  cli_hash[:input_file]
 
-  unless args.named["offline"]?
+  unless input_file && !input_file.empty?
     ClusterTools.install
   end
-  # File.write("cluster_tools.yml", CLUSTER_TOOLS)
-  # KubectlClient::Apply.file("cluster_tools.yml")
-  # ClusterTools.wait_for_cluster_tools
-  # pod_ready = ""
-  # pod_ready_timeout = 45
-  # until (pod_ready == "true" || pod_ready_timeout == 0)
-  #   pod_ready = KubectlClient::Get.pod_status("cluster-tools").split(",")[2]
-  #   Log.info { "Pod Ready Status: #{pod_ready}" }
-  #   sleep 1
-  #   pod_ready_timeout = pod_ready_timeout - 1
-  # end
-  # cluster_tools_pod = KubectlClient::Get.pod_status("cluster-tools").split(",")[0]
-  # Log.debug { "cluster_tools_pod: #{cluster_tools_pod}" }
 end
 
 desc "Uninstall CNF Test Suite Cluster Tools"
