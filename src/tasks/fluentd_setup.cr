abd5bb2616d6a4c768318625c1b07064a63cc8a8
Akash Manohar <akash@akash.im>
2022-02-16 09:50:02 +0530

    Move FluentDManager.install/uninstall to FluentD module

--- src/tasks/fluentd_setup.cr
+++ src/tasks/fluentd_setup.cr
@@ -5,30 +5,10 @@ require "totem"
 
 desc "Install Fluentd"
 task "install_fluentd" do |_, args|
-  FluentDManager.install
+  FluentD.install
 end
 
 desc "Uninstall Fluentd"
 task "uninstall_fluentd" do |_, args|
-  FluentDManager.uninstall
+  FluentD.uninstall
 end
-
-module FluentDManager
-  def self.uninstall
-    Log.for("verbose").info { "uninstall_fluentd" } 
-    Helm.delete("fluentd")
-  end
-
-  def self.install
-    #todo use embedded file to install fluentd values over fluent helm
-    #chart
-    Log.info {"Installing FluentD daemonset "}
-    File.write("fluentd-values.yml", FLUENTD_VALUES)
-    Helm.helm_repo_add("fluent","https://fluent.github.io/helm-charts")
-    #todo  #helm install --values ./override.yml fluentd ./fluentd
-    Helm.install("--values ./fluentd-values.yml fluentd fluent/fluentd")
-    KubectlClient::Get.resource_wait_for_install("Daemonset", "fluentd")
-  end
-
-end
-
--- src/tasks/utils/fluentd.cr
+++ src/tasks/utils/fluentd.cr
@@ -1,4 +1,21 @@
 module FluentD 
+  def self.install
+    #todo use embedded file to install fluentd values over fluent helm
+    #chart
+    Log.info {"Installing FluentD daemonset "}
+    File.write("fluentd-values.yml", FLUENTD_VALUES)
+    Helm.helm_repo_add("fluent","https://fluent.github.io/helm-charts")
+
+    # Install fluentd in the cnf-testsuite namespace
+    Helm.install("--values ./fluentd-values.yml -n #{TESTSUITE_NAMESPACE} fluentd fluent/fluentd")
+    KubectlClient::Get.resource_wait_for_install("Daemonset", "fluentd", namespace: TESTSUITE_NAMESPACE)
+  end
+
+  def self.uninstall
+    Log.for("verbose").info { "uninstall_fluentd" }
+    Helm.delete("fluentd -n #{TESTSUITE_NAMESPACE}")
+  end
+
   def self.match()
     ClusterTools.local_match_by_image_name("fluent/fluentd-kubernetes-daemonset")
   end
@@ -10,8 +27,9 @@ module FluentD
   #
   # todo check if fluentd installed (if not, skip)
   def self.installed?
-    KubectlClient::Get.resource_wait_for_install("Daemonset", "fluentd")
+    KubectlClient::Get.resource_wait_for_install("Daemonset", "fluentd", namespace: TESTSUITE_NAMESPACE)
   end
+
   # todo check fluentd log to see if container of application is being
   # tailed
   def self.app_tailed_by_fluentd?(pod_name, match=nil)
