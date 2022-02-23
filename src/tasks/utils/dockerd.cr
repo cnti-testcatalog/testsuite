53d563f17454a3c1d94b7f92874c3f2f49d2224f
Akash Manohar <akash@akash.im>
2022-02-23 13:39:03 +0530

    Add force_output option to dockerd

--- src/tasks/utils/dockerd.cr
+++ src/tasks/utils/dockerd.cr
@@ -15,8 +15,8 @@ module Dockerd
     KubectlClient::Delete.file(manifest_file, namespace: TESTSUITE_NAMESPACE)
   end
 
-  def self.exec(cli)
-    KubectlClient.exec("dockerd -t -- #{cli}", namespace: TESTSUITE_NAMESPACE)
+  def self.exec(cli, force_output : Bool = false)
+    KubectlClient.exec("dockerd -t -- #{cli}", namespace: TESTSUITE_NAMESPACE, force_output: force_output)
   end
 
   def self.manifest_file
