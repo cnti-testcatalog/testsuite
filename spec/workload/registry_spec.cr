e73e0712216c5db93c2a783191a0633604114544
Akash Manohar <akash@akash.im>
2022-02-24 00:11:27 +0530

    Use default namespace for the registry spec

--- spec/workload/registry_spec.cr
+++ spec/workload/registry_spec.cr
@@ -9,28 +9,29 @@ require "sam"
 
 describe "Private Registry: Image" do
   before_all do
-    Dockerd.install
+    install_dockerd = KubectlClient::Apply.file("#{TOOLS_DIR}/dockerd/manifest.yml")
     install_registry = KubectlClient::Apply.file("#{TOOLS_DIR}/registry/manifest.yml")
     KubectlClient::Get.resource_wait_for_install("Pod", "registry")
+    KubectlClient::Get.resource_wait_for_install("Pod", "dockerd")
 
-    Dockerd.exec("apk add curl", force_output: true)
-    Dockerd.exec("curl http://example.com", force_output: true)
+    KubectlClient.exec("dockerd -t -- apk add curl", force_output: true)
+    KubectlClient.exec("dockerd -t -- curl http://example.com", force_output: true)
 
     if ENV["DOCKERHUB_USERNAME"]? && ENV["DOCKERHUB_PASSWORD"]?
-      result = Dockerd.exec("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD", force_output: true)
+      result = KubectlClient.exec("dockerd -t -- docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD", force_output: true)
       Log.info { "Docker Login output: #{result[:output]}" }
     else
       puts "DOCKERHUB_USERNAME & DOCKERHUB_PASSWORD Must be set.".colorize(:red)
       exit 1
     end
 
-    Dockerd.exec("docker pull coredns/coredns:1.6.7", force_output: true)
-    Dockerd.exec("docker tag coredns/coredns:1.6.7 registry:5000/coredns:1.6.7", force_output: true)
-    Dockerd.exec("docker push registry:5000/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker pull coredns/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker tag coredns/coredns:1.6.7 registry:5000/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker push registry:5000/coredns:1.6.7", force_output: true)
 
     # This is required for the test that uses the sample_local_registry_org_image CNF
-    Dockerd.exec("docker tag coredns/coredns:1.6.7 registry:5000/coredns-sample-org/coredns:1.6.7", force_output: true)
-    Dockerd.exec("docker push registry:5000/coredns-sample-org/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker tag coredns/coredns:1.6.7 registry:5000/coredns-sample-org/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker push registry:5000/coredns-sample-org/coredns:1.6.7", force_output: true)
   end
 
   it "'reasonable_image_size' should pass if using local registry and a port", tags: ["private_registry_image"]  do
@@ -44,8 +45,6 @@ describe "Private Registry: Image" do
   ensure
     LOGGING.info `./cnf-testsuite cnf_cleanup cnf-path=#{cnf}`
   end
-
-
 
   it "'reasonable_image_size' should pass if using local registry, a port and an org", tags: ["private_registry_image"]  do
     cnf="./sample-cnfs/sample_local_registry_org_image"
@@ -61,26 +60,27 @@ describe "Private Registry: Image" do
 
   after_all do
     delete_registry = KubectlClient::Delete.file("#{TOOLS_DIR}/registry/manifest.yml")
-    Dockerd.uninstall
+    delete_dockerd = KubectlClient::Delete.file("#{TOOLS_DIR}/dockerd/manifest.yml")
   end	
 end
 
 describe "Private Registry: Rolling" do
   before_all do
-    Dockerd.install
+    install_dockerd = KubectlClient::Apply.file("#{TOOLS_DIR}/dockerd/manifest.yml")
     install_registry = KubectlClient::Apply.file("#{TOOLS_DIR}/registry/manifest.yml")
     KubectlClient::Get.resource_wait_for_install("Pod", "registry")
+    KubectlClient::Get.resource_wait_for_install("Pod", "dockerd")
 
-    Dockerd.exec("apk add curl", force_output: true)
-    Dockerd.exec("curl http://example.com", force_output: true)
+    KubectlClient.exec("dockerd -t -- apk add curl", force_output: true)
+    KubectlClient.exec("dockerd -t -- curl http://example.com", force_output: true)
 
-    Dockerd.exec("docker pull coredns/coredns:1.6.7", force_output: true)
-    Dockerd.exec("docker tag coredns/coredns:1.6.7 registry:5000/coredns:1.6.7", force_output: true)
-    Dockerd.exec("docker push registry:5000/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker pull coredns/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker tag coredns/coredns:1.6.7 registry:5000/coredns:1.6.7", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker push registry:5000/coredns:1.6.7", force_output: true)
 
-    Dockerd.exec("docker pull coredns/coredns:1.8.0", force_output: true)
-    Dockerd.exec("docker tag coredns/coredns:1.8.0 registry:5000/coredns:1.8.0", force_output: true)
-    Dockerd.exec("docker push registry:5000/coredns:1.8.0", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker pull coredns/coredns:1.8.0", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker tag coredns/coredns:1.8.0 registry:5000/coredns:1.8.0", force_output: true)
+    KubectlClient.exec("dockerd -t -- docker push registry:5000/coredns:1.8.0", force_output: true)
   end
 
   it "'rolling_update' should pass if using local registry and a port", tags: ["private_registry_rolling"]  do
@@ -127,6 +127,6 @@ describe "Private Registry: Rolling" do
 
   after_all do
     delete_registry = KubectlClient::Delete.file("#{TOOLS_DIR}/registry/manifest.yml")
-    Dockerd.uninstall
+    delete_dockerd = KubectlClient::Delete.file("#{TOOLS_DIR}/dockerd/manifest.yml")
   end	
 end
