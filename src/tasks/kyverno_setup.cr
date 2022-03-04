9a618cb169d14ff85641056ac0d45fdf383de1e6
Akash Manohar <akash@akash.im>
2022-03-04 10:26:54 +0530

    Cleanup kyverno_setup.cr
    * Remove commented lines
    * Add Kyverno install failure message
    * Remove unused variables

--- src/tasks/kyverno_setup.cr
+++ src/tasks/kyverno_setup.cr
@@ -7,19 +7,13 @@ require "./utils/utils.cr"
 desc "Install Kyverno"
 task "install_kyverno" do |_, args|
   kyverno_version="1.5.0"
-  stdout = IO::Memory.new
-  stderr = IO::Memory.new
   url = "https://raw.githubusercontent.com/kyverno/kyverno/v#{kyverno_version}/definitions/release/install.yaml"
-#  url = "https://raw.githubusercontent.com/nsagark/nginx/main/install.yaml?token=AVOW2YXCWWYMYX7PFYJ6ZBLBU7ZQC"
   result = KubectlClient::Apply.file(url)
-
-  if !result[:status].success?
-    # fail and exit
-  end
-
   is_ready = KubectlClient::Get.resource_wait_for_install("deployment", "kyverno", 180, "kyverno")
   if is_ready
     stdout_success "Kyverno successfully installed"
+  else
+    stdout_success "Kyverno installation failed"
   end
 end
 
@@ -27,14 +21,11 @@ end
 desc "Uninstall Kyverno"
 task "uninstall_kyverno" do |_, args|
   kyverno_version="1.5.0"
-  stdout = IO::Memory.new
-  stderr = IO::Memory.new
-   url = "https://raw.githubusercontent.com/kyverno/kyverno/v#{kyverno_version}/definitions/release/install.yaml"
-#   url = "https://raw.githubusercontent.com/nsagark/nginx/main/install.yaml?token=AVOW2YXCWWYMYX7PFYJ6ZBLBU7ZQC"
+  url = "https://raw.githubusercontent.com/kyverno/kyverno/v#{kyverno_version}/definitions/release/install.yaml"
   result = KubectlClient::Delete.file(url)
 
   if !result[:status].success?
-    stdout_failure "Kyverno could not uninstall correctly. Please uninstall manually"
+    stdout_failure "Kyverno could not be uninstalled."
   else
     stdout_success "Kyverno was uninstalled successfully"
   end
@@ -42,9 +33,6 @@ end
 
 desc "Delete all cluster policies and policy reports in all namespaces"
 task "cleanup_policies" do |_, args|
-  stdout = IO::Memory.new
-  stderr = IO::Memory.new
-
   result = KubectlClient::Get.delete_all_clusterpolicies_and_policyreports_allnamespaces()
 
   if !result[:status].success?
