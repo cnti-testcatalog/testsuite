49365932a3c67be40b419d75ab8f708a0f8ff08f
agentpoyo <drew.bentley@gmail.com>
2022-03-09 15:23:29 -0500

    Add env var for RUNNER_IMAGE in create_runners.sh script

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -29,11 +29,12 @@ VIPS=(
 
 RUNNER_COUNT=0
 for node in "${!RUNNERS[@]}"; do
-    ssh root@${RUNNERS[$node]} "docker pull conformance/github-runner:2.288.1"
+    export RUNNER_IMAGE="conformance/github-runner:2.288.1"
+    ssh root@${RUNNERS[$node]} "docker pull $RUNNER_IMAGE"
     RUNNERS_PER_NODE=16
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
         ssh root@${RUNNERS[$node]} "docker rm --force github-runner$RUNNER_COUNT"
-        ssh root@${RUNNERS[$node]} "docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cncf/cnf-testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared conformance/github-runner:2.287.1"
+        ssh root@${RUNNERS[$node]} "docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cncf/cnf-testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
         RUNNERS_PER_NODE=$(($RUNNERS_PER_NODE - 1))
     done
