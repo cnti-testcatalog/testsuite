cf8282c0e969a88563131f8929863cd41b023df9
agentpoyo <drew.bentley@gmail.com>
2022-03-09 15:04:54 -0500

    Remove docker rm from cleanup_runner.sh

--- tools/github-runner/cleanup_runners.sh
+++ tools/github-runner/cleanup_runners.sh
@@ -21,7 +21,6 @@ for node in "${RUNNERS[@]}"; do
     ssh root@$node docker exec ${RUNNER_CONTAINERS[1]} "sh -c 'kind delete clusters \$(kind get clusters)'"
     for runner in  "${RUNNER_CONTAINERS[@]}"; do
       ssh root@$node docker exec $runner "/actions-runner/bin/Runner.Listener remove --token $TOKEN"
-      ssh root@$node docker rm --force $runner
     done
     ssh root@$node "systemctl stop docker"
     ssh root@$node "rm -rf /var/lib/docker/*"
