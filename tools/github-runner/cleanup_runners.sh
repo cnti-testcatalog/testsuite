c62bd77a84b1fbf88f93f92bea768c09266af05e
Akash Manohar <akash@akash.im>
2022-03-10 00:47:46 +0530

    Force-delete containes during cleanupr to prevent name clashes when recreating the runners

--- tools/github-runner/cleanup_runners.sh
+++ tools/github-runner/cleanup_runners.sh
@@ -20,7 +20,8 @@ for node in "${RUNNERS[@]}"; do
     RUNNER_CONTAINERS=( $(ssh root@$node docker ps --filter "name=github-runner" --format '{{.Names}}' | xargs) )
     ssh root@$node docker exec ${RUNNER_CONTAINERS[1]} "sh -c 'kind delete clusters \$(kind get clusters)'"
     for runner in  "${RUNNER_CONTAINERS[@]}"; do
-        ssh root@$node docker exec $runner "/actions-runner/bin/Runner.Listener remove --token $TOKEN"
+      ssh root@$node docker exec $runner "/actions-runner/bin/Runner.Listener remove --token $TOKEN"
+      ssh root@$node docker rm --force $runner
     done
     ssh root@$node "systemctl stop docker"
     ssh root@$node "rm -rf /var/lib/docker/*"
