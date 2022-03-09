df237bf67e23d590d69ced4b41025b153b3a4fed
Akash Manohar <akash@akash.im>
2022-03-10 00:25:12 +0530

    Script to delete github runners

---
+++ tools/github-runner/delete-runners-on-github.sh
@@ -0,0 +1,15 @@
+####
+# Requirements:
+#   Install "jq" to work with json in the shell
+#
+# USAGE:
+#   GH_USER=TODO GH_TOKEN=TODO bash tools/github-runner/delete-runners-on-github.sh
+#
+
+curl -u $GH_USER:$GH_TOKEN -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/cncf/cnf-testsuite/actions/runners?per_page=100 | \
+jq -r '.runners[].id' | \
+while IFS='' read -r line; do
+  echo "Calling url https://api.github.com/repos/cncf/cnf-testsuite/actions/runners/$line"
+  curl -X DELETE -u $GH_USER:$GH_TOKEN -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/cncf/cnf-testsuite/actions/runners/$line"
+done
