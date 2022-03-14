7b90f218c6c84a4445fa0df66d3e75be2ec0acbe
Akash Manohar <akash@akash.im>
2022-03-14 21:33:51 +0530

    Add logging statements for Kyverno policy audit run

--- src/tasks/utils/kyverno.cr
+++ src/tasks/utils/kyverno.cr
@@ -88,7 +88,9 @@ module Kyverno
 
     def self.run(policy_path : String, exclude_namespaces : Array(String) = [] of String)
       cmd = "#{Kyverno.binary_path} apply #{policy_path} --cluster --policy-report"
-      result = ShellCmd.run(cmd, "", force_output: true)
+      ShellCmd.run("ls #{policy_path}", "kyverno_policy_path", force_output: true)
+      result = ShellCmd.run(cmd, "Kyverno::PolicyAudit.run", force_output: true)
+      Log.for("kyverno_audit").info { result[:output] }
       policy_report_yaml = result[:output].split("\n")[6..-1].join("\n")
       policy_report = YAML.parse(policy_report_yaml)
 
@@ -114,79 +116,4 @@ module Kyverno
     end
   end
 
-  module PolicyReport
-    struct FailedResource
-      property kind
-      property name
-      property namespace
-
-      def initialize(@kind : String, @name : String, @namespace : String)
-      end
-    end
-
-    struct PolicyFailure
-      property message
-      property resources
-
-      def initialize(@message : String, @resources : Array(FailedResource))
-      end
-    end
-
-    def self.failures(policy_name : String, exclude_namespaces : Array(String) = [] of String) : Array(PolicyFailure)
-      result = Kyverno::PolicyReport.all()
-      policy_reports = JSON.parse(result[:output])
-
-      failures = [] of PolicyFailure
-      policy_reports["items"].as_a.each do |policy_report|
-        report_namespace = policy_report["metadata"]["namespace"]
-        if !exclude_namespaces.includes?(report_namespace)
-          policy_report["results"].as_a.each do |test_result|
-            if test_result["result"] == "fail" && test_result["policy"] == policy_name
-              failed_resources = test_result["resources"].as_a.map { |r| FailedResource.new(r["kind"].to_s, r["name"].to_s, r["namespace"].to_s) }
-              policy_failure = PolicyFailure.new(test_result["message"].to_s, failed_resources)
-              failures.push(policy_failure)
-            end
-          end
-        end
-      end
-
-      Log.for("kyverno_failures for #{policy_name}").info { failures.inspect }
-      failures
-    end
-
-    def self.all()
-      cmd = "kubectl get polr -A -o json"
-      ShellCmd.run(cmd, "Kyverno::PolicyReport.all")
-    end
-
-    def self.get(name : String)
-      cmd = "kubectl get polr #{name} -o json"
-      ShellCmd.run(cmd, "Kyverno::PolicyReport.get")
-    end
-
-    def self.delete_all()
-      cmd = "kubectl delete polr --all -A"
-      result = ShellCmd.run(cmd, "Kyverno::PolicyReport.delete_all")
-      result[:status].success?
-    end
-  end
-
-  module ClusterPolicy
-    def self.all()
-      cmd = "kubectl get cpol -o json"
-      ShellCmd.run(cmd, "Kyverno::ClusterPolicy.all")
-    end
-
-    def self.delete_all()
-      cmd = "kubectl delete cpol --all -A"
-      result = ShellCmd.run(cmd, "Kyverno::ClusterPolicy.delete_all")
-      result[:status].success?
-    end
-  end
-
-  # NOTE: Not used anywhere. Retaining until the entire Kyverno PR is merged.
-  # def self.policy_report_failed(name : String)
-  #   cmd = "kubectl get polr -A -o yaml | grep \"result: fail\" -B10 | grep #{name} -B2 -A7"
-  #   ShellCmd.run(cmd, "KubectlClient::Get.policy_report_failed")
-  # end
 end
