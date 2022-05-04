444672b74cb92f35e01eff5efd3cd3ad483f83a5
Akash Manohar <akash@akash.im>
2022-05-04 18:17:42 +0530

    Add app_namespace support for node_drain chaos template

--- src/tasks/utils/chaos_templates.cr
+++ src/tasks/utils/chaos_templates.cr
@@ -101,6 +101,7 @@ class ChaosTemplates
     def initialize(
       @test_name : String,
       @chaos_experiment_name : String,
+      @app_namespace : String,
       @deployment_label : String,
       @deployment_label_value : String,
       @total_chaos_duration : String,
