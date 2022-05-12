1ee17fd09488863e8efb7269bc47cb1d967aacd3
W. Watson <w.watson@vulk.coop>
2022-05-12 14:28:35 -0400

    #1448 total passed now uses multiple tags

--- src/tasks/cert/cert_observability.cr
+++ src/tasks/cert/cert_observability.cr
@@ -8,7 +8,8 @@ require "../utils/utils.cr"
 desc "In order to maintain, debug, and have insight into a protected environment, its infrastructure elements must have the property of being observable. This means these elements must externalize their internal states in some way that lends itself to metrics, tracing, and logging."
 task "cert_observability", ["cert_observability_title", "log_output", "prometheus_traffic", "open_metrics", "routed_logs", "tracing"] do |_, args|
 # task "cert_observability", ["cert_observability_title", "prometheus_traffic", "open_metrics", "routed_logs", "tracing"] do |_, args|
-  stdout_score("observability", "Observability and Diagnostics")
+  # stdout_score("observability", "Observability and Diagnostics")
+  stdout_score(["observability", "cert"], "Observability and Diagnostics")
 end
 
 task "cert_observability_title" do |_, args|
