1b740f77f6a389ad63539300374fc95b1fb25c14
Akash Manohar <akash@akash.im>
2022-05-30 20:10:21 +0530

    [#1478] Update pod_dns_error to support non-default namespaces
    Other squashed changes
    * Re-raise exception and let ensure cleanup the CNF and litmus installation
    * Remove Log.info wrapper for backtick cmd in pod_dns_error spec
    
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/workload/resilience/pod_dns_error_spec.cr
+++ spec/workload/resilience/pod_dns_error_spec.cr
@@ -14,17 +14,21 @@ describe "Resilience pod dns error Chaos" do
 
   it "'pod_dns_error' A 'Good' CNF should not crash when pod dns error occurs", tags: ["pod_dns_error"]  do
     begin
-     Log.info { `./cnf-testsuite cnf_setup cnf-config=example-cnfs/envoy/cnf-testsuite.yml`}
+      `./cnf-testsuite cnf_setup cnf-config=example-cnfs/envoy/cnf-testsuite.yml`
       $?.success?.should be_true
       response_s = `./cnf-testsuite pod_dns_error verbose`
       Log.info { response_s }
       $?.success?.should be_true
       ((/SKIPPED: pod_dns_error docker runtime not found/)  =~ response_s || 
        (/PASSED: pod_dns_error chaos test passed/ =~ response_s)).should_not be_nil
+    rescue ex
+      # Raise back error to ensure test fails.
+      # The ensure block will cleanup the CNF and the litmus installation.
+      raise "Test failed with #{ex.message}"
     ensure
-      Log.info {`./cnf-testsuite cnf_cleanup cnf-config=example-cnfs/envoy/cnf-testsuite.yml`}
+      `./cnf-testsuite cnf_cleanup cnf-config=example-cnfs/envoy/cnf-testsuite.yml`
       $?.success?.should be_true
-      Log.info {`./cnf-testsuite uninstall_litmus`}
+      `./cnf-testsuite uninstall_litmus`
       $?.success?.should be_true
     end
   end
