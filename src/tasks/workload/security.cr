ba5e6c151181c994fbed2e303027f81bf84da320
Akash Manohar <akash@akash.im>
2022-06-27 17:07:31 +0530

    Update immutable_file_systems to use new Kubescape helpers
    Signed-off-by: Akash Manohar <akash@akash.im>

--- src/tasks/workload/security.cr
+++ src/tasks/workload/security.cr
@@ -520,14 +520,17 @@ task "immutable_file_systems", ["kubescape_scan"] do |_, args|
     Log.for("verbose").info { "immutable_file_systems" } if check_verbose(args)
     results_json = Kubescape.parse
     test_json = Kubescape.test_by_test_name(results_json, "Immutable container filesystem")
+    test_report = Kubescape.parse_test_report(test_json)
+    resource_keys = CNFManager.workload_resource_keys(args, config)
+    test_report = Kubescape.filter_cnf_resources(test_report, resource_keys)
 
     emoji_security = "🔓🔑"
-    if Kubescape.test_passed?(test_json)
+    if test_report.failed_resources.size == 0
       upsert_passed_task("immutable_file_systems", "✔️  PASSED: Containers have immutable file systems #{emoji_security}")
     else
       resp = upsert_failed_task("immutable_file_systems", "✖️  FAILED: Found containers with mutable file systems #{emoji_security}")
-      Kubescape.alerts_by_test(test_json).map{|t| puts "\n#{t}".colorize(:red)}
-      puts "Remediation: #{Kubescape.remediation(test_json)}\n".colorize(:red)
+      test_report.failed_resources.map {|r| stdout_failure(r.alert_message) }
+      stdout_failure("Remediation: #{test_report.remediation}")
       resp
     end
   end
