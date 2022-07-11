f3dd71a760647d0174caa22a62840dcf6e6f8f86
Akash Manohar <akash@akash.im>
2022-07-11 21:26:41 +0700

    Import fluentbit module in spec file
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/workload/observability_spec.cr
+++ spec/workload/observability_spec.cr
@@ -1,6 +1,7 @@
 require "../spec_helper"
 require "colorize"
 require "../../src/tasks/utils/utils.cr"
+require "../../src/tasks/utils/fluentbit.cr"
 require "../../src/tasks/jaeger_setup.cr"
 
 describe "Observability" do
