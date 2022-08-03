9a2ebfdf5f696d5f44bedd58460c7417e9c9e0ae
Akash Manohar <akash@akash.im>
2022-08-03 10:23:07 +0530

    [#1593] Install OPA Gatekeeper to different namespace
    Signed-off-by: Akash Manohar <akash@akash.im>

--- src/tasks/opa_setup.cr
+++ src/tasks/opa_setup.cr
@@ -12,11 +12,11 @@ task "install_opa" do |_, args|
   if args.named["offline"]?
     LOGGING.info "Intalling OPA Gatekeeper in Offline Mode"
     chart = Dir.entries(OPA_OFFLINE_DIR).first
-    Helm.install("--set auditInterval=1 --set postInstall.labelNamespace.enabled=false opa-gatekeeper #{OPA_OFFLINE_DIR}/#{chart}")
+    Helm.install("--set auditInterval=1 --set postInstall.labelNamespace.enabled=false opa-gatekeeper #{OPA_OFFLINE_DIR}/#{chart} -n #{TESTSUITE_NAMESPACE}")
   else
     Helm.helm_repo_add("gatekeeper", "https://open-policy-agent.github.io/gatekeeper/charts")
     begin
-      Helm.install("--set auditInterval=1 --set postInstall.labelNamespace.enabled=false opa-gatekeeper gatekeeper/gatekeeper")
+      Helm.install("--set auditInterval=1 --set postInstall.labelNamespace.enabled=false opa-gatekeeper gatekeeper/gatekeeper -n #{TESTSUITE_NAMESPACE}")
     rescue e : Helm::CannotReuseReleaseNameError
       stdout_warning "gatekeeper already installed"
     end
