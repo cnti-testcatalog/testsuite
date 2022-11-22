cee4f6f250e63c109a3b4b9bd3b6fb5456f9f1cb
Akash Manohar <akash@akash.im>
2022-11-22 13:55:38 +0700

    [bug/1697] Update assertion in cnf_manager spec
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/utils/cnf_manager_spec.cr
+++ spec/utils/cnf_manager_spec.cr
@@ -471,7 +471,7 @@ describe "SampleUtils" do
       Log.info { install }
       response_s = `./cnf-testsuite cert_microservice`
       Log.info { response_s}
-      (/of 4 tests passed/ =~ response_s).should_not be_nil
+      (/of 3 tests passed/ =~ response_s).should_not be_nil
       Log.info { response_s }
     ensure
       Log.info { `./cnf-testsuite cnf_cleanup cnf-path=./sample-cnfs/sample-ndn-privileged` }
