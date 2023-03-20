c490b7678dc67cdb13ad0aa68dce029d56b3605e
Akash Manohar <akash@akash.im>
2023-03-20 21:15:19 +0700

    Update crystal install in dockerfile
    Signed-off-by: Akash Manohar <akash@akash.im>

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -29,7 +29,7 @@ VIPS=(
 
 RUNNER_COUNT=0
 for node in "${!RUNNERS[@]}"; do
-    export RUNNER_IMAGE="conformance/github-runner:v2.303.0-2" # don't forget the v
+    export RUNNER_IMAGE="conformance/github-runner:v2.303.0-3" # don't forget the v
     ssh root@${RUNNERS[$node]} "docker pull $RUNNER_IMAGE"
     RUNNERS_PER_NODE=4
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
