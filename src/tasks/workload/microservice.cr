fe17a71abd35ba5dcfbf95f21f08c05ae975d9b0
Akash Manohar <akash@akash.im>
2023-09-08 15:25:05 +0700

    Fix has_key typo

--- src/tasks/workload/microservice.cr
+++ src/tasks/workload/microservice.cr
@@ -518,7 +518,7 @@ task "sig_term_handled" do |_, args|
             sig_result = container_statuses.map do |container_status|
               container_name = container_status.dig("name")
               previous_process_type = "initial_name"
-              if container_status.has_key?("containerID")
+              if container_status.as_h.has_key?("containerID")
                 Log.for("sigterm_fail_dbg_pass").info { container_status.inspect }
               else
                 Log.for("sigterm_fail_dbg_fail").info { container_status.inspect }
