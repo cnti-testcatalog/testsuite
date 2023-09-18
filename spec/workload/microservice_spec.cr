9307a16cb4aaa7ebfb1e48fe28ac3eaee948468d
Akash Manohar <akash@akash.im>
2023-09-18 10:24:46 +0700

    Wait for jenkins pod to be ready
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/workload/microservice_spec.cr
+++ spec/workload/microservice_spec.cr
@@ -273,8 +273,8 @@ describe "Microservice" do
       #todo 4. Make sure that threads are not counted as new processes.  A thread does not get a signal (sigterm or sigkill)
       Log.info { `./cnf-testsuite cnf_setup cnf-path=./sample-cnfs/sample_good_signal_handling_tini/` }
 
-      # workaround to wait for pod because Jenkins takes a LONG time to start.
-      Log.info { `kubectl wait --for=condition=complete pod/jenkins-0 -n cnfspace` }
+      # Workaround to wait using kubectl because Jenkins pod takes a LONG time to start.
+      Log.info { `kubectl wait --for=condition=ready=True pod/jenkins-0 -n cnfspace --timeout=500s` }
 
       response_s = `./cnf-testsuite sig_term_handled verbose`
       Log.info { response_s }
