56a3d59cdd6a92c20b6d79405e54d1f7d2fbac72
denverwilliams <denver@debian.nz>
2024-02-01 15:46:41 +0000

    updated runner ip and vip with new repo url

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -3,37 +3,21 @@
 RUNNERS=(
     136.144.55.87
     136.144.55.243
-    139.178.69.151
-    139.178.69.153
-    139.178.68.167
-    86.109.1.243
-    139.178.68.111
-    139.178.68.103
-    139.178.68.93
-    139.178.68.107
-    136.144.54.249)
+    139.178.69.151)
 
 VIPS=(
-    147.75.89.176/28
-    147.75.108.64/28
-    147.75.202.0/28
-    147.75.202.80/28
-    147.75.88.0/28
-    147.75.202.112/28
-    147.75.203.112/28
-    147.75.203.144/28
-    147.75.203.160/28
-    147.75.203.176/28
-    147.75.203.192/28)
+    147.75.202.1/28
+    147.75.108.65/28
+    147.75.89.177/28)
 
 
 RUNNER_COUNT=0
 for node in "${!RUNNERS[@]}"; do
     export RUNNER_IMAGE="conformance/github-runner:v2.303.0-4" # don't forget the v
     ssh root@${RUNNERS[$node]} "docker pull $RUNNER_IMAGE"
-    RUNNERS_PER_NODE=4
+    RUNNERS_PER_NODE=10
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
-        ssh root@${RUNNERS[$node]} "docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cncf/cnf-testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
+        ssh root@${RUNNERS[$node]} "docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
         RUNNERS_PER_NODE=$(($RUNNERS_PER_NODE - 1))
     done
