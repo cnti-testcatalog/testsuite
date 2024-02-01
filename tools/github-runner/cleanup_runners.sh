cd164a9247f1f8a01d1c8f134b2f52ed8ae7a70e
Drew Bentley <drew.bentley@gmail.com>
2024-02-01 16:09:21 +0000

    added TOKEN argument for script to simplify and not run until theres a token

--- tools/github-runner/cleanup_runners.sh
+++ tools/github-runner/cleanup_runners.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+TOKEN=$1
+
 RUNNERS=(
     136.144.55.87
     136.144.55.243
     139.178.69.151)
+
+
+if [ -z "$1" ]; then
+   echo "USAGE: ./cleanup_runners.sh <GITHUB_RUNNER_TOKEN>"
+   exit 1
+fi
 
 #TOKEN=$(curl  -XPOST -H "authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/cncf/cnf-testsuite/actions/runners/registration-token | jq -r .token)
 
