bd39961a8e9062100bd34f8746d00341ff6f2137
W. Watson <w.watson@vulk.coop>
2024-02-06 21:08:52 +0000

    #1806 puts now logs

--- src/tasks/workload/5g_validator.cr
+++ src/tasks/workload/5g_validator.cr
@@ -21,7 +21,12 @@ task "smf_upf_core_validator" do |t, args|
     task_start_time = Time.utc
     testsuite_task = "smf_upf_core_validator"
     Log.for(testsuite_task).info { "Starting test" }
+
+		# todo add other resilience and compatiblity tests
+
     args.named["reslience_tests"]="pod_network_latency, pod_delete"
+
+		# todo find heartbeat for ran
     t.invoke("smf_upf_heartbeat", args)
   end
 end
