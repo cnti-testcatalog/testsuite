7a9a943faf6b4e4dcd8df9e9fb0056005d657565
Drew Bentley <drew.bentley@gmail.com>
2024-02-06 15:37:11 +0000

    Changed runners back to 4 each

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -22,7 +22,7 @@ RUNNER_COUNT=0
 for node in "${!RUNNERS[@]}"; do
     export RUNNER_IMAGE="conformance/github-runner:v2.303.0-4" # don't forget the v
     ssh root@${RUNNERS[$node]} "docker pull $RUNNER_IMAGE"
-    RUNNERS_PER_NODE=10
+    RUNNERS_PER_NODE=4
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
         ssh root@${RUNNERS[$node]} "docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
