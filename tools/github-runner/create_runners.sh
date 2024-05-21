d852550c200a6b680ae2c62d71baa25d1f95a7a2
denverwilliams <denver@debian.nz>
2024-05-21 23:02:21 +0000

    Update runner scripts

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -11,7 +11,7 @@ fi
 RUNNER_COUNT=0
 RUNNER_IMAGE="conformance/github-runner:v2.316.1" # don't forget the v
 docker pull $RUNNER_IMAGE
-RUNNERS_PER_NODE=20
+RUNNERS_PER_NODE=10
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
         docker run -d --network host --restart always --name unh-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="unhrunner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/unh-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
