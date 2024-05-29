9a3ef67d95fcd3b35d90315e0db3e6ce4fd5a5e0
denverwilliams <denver@debian.nz>
2024-05-29 04:52:38 +0000

    Update Runner Deployment

--- tools/github-runner/cleanup_runners.sh
+++ tools/github-runner/cleanup_runners.sh
@@ -2,21 +2,41 @@
 
 TOKEN=$1
 
+RUNNERS=(
+	2001
+        2002
+        2003
+        2004
+        2005
+        2006
+        2007
+        2008
+        2009)
+
+
+
 if [ -z "$1" ]; then
    echo "USAGE: ./cleanup_runners.sh <GITHUB_RUNNER_TOKEN>"
    exit 1
 fi
 
+eval $(ssh-agent)
+ssh-add /home/pair/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-mantic64/20240514.0.0/virtualbox/vagrant_insecure_key
+
 #TOKEN=$(curl  -XPOST -H "authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/cncf/cnf-testsuite/actions/runners/registration-token | jq -r .token)
 
-RUNNER_CONTAINERS=( $(docker ps --filter "name=unh-runner" --format '{{.Names}}' | xargs) )
-docker exec ${RUNNER_CONTAINERS[1]} "sh -c 'kind delete clusters \$(kind get clusters)'"
-for runner in  "${RUNNER_CONTAINERS[@]}"; do
-   docker exec $runner /actions-runner/bin/Runner.Listener remove --token $TOKEN
+for node in "${!RUNNERS[@]}"; do
+
+    RUNNER_CONTAINERS=( $(ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} sudo docker ps --filter "name=github-runner" --format '{{.Names}}' | xargs) )
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo docker rm -f \$(docker ps -a -q)"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} sudo docker exec ${RUNNER_CONTAINERS[1]} "sh -c 'kind delete clusters \$(kind get clusters)'"
+    for runner in  "${RUNNER_CONTAINERS[@]}"; do
+      ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} sudo docker exec $runner "/actions-runner/bin/Runner.Listener remove --token $TOKEN"
+    done
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo systemctl stop docker"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo rm -rf /var/lib/docker/*"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo systemctl start docker"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo rm -rf /tmp/*"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo rm -rf /shared/"
+    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo rm -rf /runner-tmp/"
 done
-sudo systemctl stop docker
-sudo rm -rf /var/lib/docker/*
-sudo systemctl start docker
-sudo rm -rf /tmp/*
-sudo rm -rf /shared/
-sudo rm -rf /runner-tmp/
--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-#	2001
 RUNNERS=(
-        2002
-        2003
-        2004
+	2001
+	2002
+	2003
+	2004
         2005
         2006
         2007
         2008
-        2009
-        2010
-        2011
-        2012
-        2013
-        2014
-        2015
-        2016
-        2017
-        2018)
+        2009)
 
 TOKEN=$1
 
@@ -34,7 +25,7 @@ RUNNER_COUNT=0
 for node in "${!RUNNERS[@]}"; do
     export RUNNER_IMAGE="conformance/github-runner:v2.316.1" # don't forget the v
     ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo docker pull $RUNNER_IMAGE"
-    RUNNERS_PER_NODE=4
+    RUNNERS_PER_NODE=2
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
         ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
