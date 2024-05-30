69682f11f40a457f21bb3c1822a82d8e6819e8de
denverwilliams <denver@debian.nz>
2024-05-30 05:10:03 +0000

    Add VM Updates

--- tools/github-runner/create_runners.sh
+++ tools/github-runner/create_runners.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-RUNNERS=(
-	2001
-	2002
-	2003
-	2004
-        2005
-        2006
-        2007
-        2008
-        2009)
-
 TOKEN=$1
 
 if [ -z "$1" ]; then
@@ -18,17 +7,15 @@ if [ -z "$1" ]; then
      exit 1
 fi
 
-eval $(ssh-agent)
-ssh-add /home/pair/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-mantic64/20240514.0.0/virtualbox/vagrant_insecure_key
 
 RUNNER_COUNT=0
-for node in "${!RUNNERS[@]}"; do
-    export RUNNER_IMAGE="conformance/github-runner:v2.316.1" # don't forget the v
-    ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo docker pull $RUNNER_IMAGE"
-    RUNNERS_PER_NODE=2
+RUNNER_IMAGE="conformance/github-runner:v2.316.1" # don't forget the v
+docker pull $RUNNER_IMAGE
+RUNNERS_PER_NODE=4
     until [ $RUNNERS_PER_NODE -eq 0 ]; do
-        ssh vagrant@127.0.0.1 -p ${RUNNERS[$node]} "sudo docker run -d --network host --restart always --name github-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="runner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/github-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE"
+        docker run -d --network host --restart always --name unh-runner$RUNNER_COUNT -e REPO_URL="https://github.com/cnti-testcatalog/testsuite" -e RUNNER_NAME="unhrunner$RUNNER_COUNT" -e RUNNER_TOKEN="$TOKEN" -e RUNNER_WORKDIR="/unh-runner-cnf-testsuite" -e LABELS="v1.0.0" -v /var/run/docker.sock:/var/run/docker.sock -v /runner-tmp/runner$RUNNER_COUNT:/tmp -v /github-runner-cnf-testsuite/cnf-testsuite/cnf-testsuite/tools:/docker-host-repo/tools -v /shared:/shared $RUNNER_IMAGE
         RUNNER_COUNT=$(($RUNNER_COUNT + 1))
         RUNNERS_PER_NODE=$(($RUNNERS_PER_NODE - 1))
     done
-done
+sudo chmod 777 /runner-tmp -R
+
