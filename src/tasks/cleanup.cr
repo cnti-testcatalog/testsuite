6cf5a94ed8d3b02f6f71fbcf8931e749c6bce2ba
svteb <slavo.valko@tietoevry.com>
2025-01-16 10:09:06 +0000

    Fix: Repair "uninstall_all" Refs: #2209 - The "uninstall_all" was broken during development of #2120, to fix the issue a failure when there is no CNF to uninstall has been relegated to a warning and exit 1 was removed. - Two spec tests have been added to avoid this scenario in the future. - Additionally some renaming was left-over from #2184 that was resolved.
    Signed-off-by: svteb <slavo.valko@tietoevry.com>

--- src/tasks/cleanup.cr
+++ src/tasks/cleanup.cr
@@ -7,8 +7,14 @@ desc "Alias for cnf_uninstall"
 task "uninstall", ["cnf_uninstall"] do  |_, args|
 end
 
+# Private task
+task "_tools_uninstall_start" do
+  stdout_success "Uninstalling testsuite helper tools."
+end
+
 desc "Cleans up the CNF Test Suite helper tools and containers"
 task "tools_uninstall", [
+    "_tools_uninstall_start",
     "uninstall_sonobuoy",
     "uninstall_chaosmesh",
     "uninstall_litmus",
@@ -20,11 +26,12 @@ task "tools_uninstall", [
     # Helm needs to be uninstalled last to allow other uninstalls to use helm if necessary.
     # Check this issue for details - https://github.com/cncf/cnf-testsuite/issues/1586
     "uninstall_local_helm"
-  ] do  |_, args|
+  ] do |_, args|
+  stdout_success "Testsuite helper tools uninstalled."
 end
 
 desc "Cleans up the CNF Test Suite sample projects, helper tools, and containers"
-task "uninstall_all", ["cnf_uninstall", "tools_uninstall"] do  |_, args|
+task "uninstall_all", ["cnf_uninstall", "tools_uninstall"] do |_, args|
 end
 
 task "delete_results" do |_, args|
